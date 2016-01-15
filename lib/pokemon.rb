8575be66ee61b53a70ddc063d765d3be38b3e2af
Dakota Martinez <dakotaleemusic@gmail.com>
2016-01-15 09:05:56 +0000

    completes sql pokemon scraper lab

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  
+  @@all = []
+  
+  def initialize(id=Pokemon.all.size+1,name, type, db)
+    @id = id
+    name = name
+    type = type
+    db = db
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.save(name, type, db)
+    ins = db.prepare("INSERT INTO pokemon (name, type) VALUES (?, ?)")
+    ins.execute(name, type)
+  end
+  
+  def self.find(id, db)
+    sel = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    sel.flatten
+  end
+    
 end
