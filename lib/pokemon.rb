9449aa98512c06509913e312c6948d81f4b60861
pacosta613 <pedro.acosta613@gmail.com>
2016-01-25 17:58:19 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+
+  def initialize(name, type, db, poke)
+    @name = name
+    @type = type
+    @db = db
+    @poke = poke
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO Pokemon (name, type) VALUES (?,?)",name, type)
+  end
+
+  def self.find(type, db)
+    db.execute("SELECT * FROM Pokemon WHERE type = 'fire'").flatten
+  end
+
 end
