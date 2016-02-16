c35dec1410a0c73b82d55ecdc872b96c59e2e9c2
Tom Johns <tommyskies@hotmail.com>
2016-02-16 14:36:24 -0700

    sanitize pokemon find method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
 	end
 	
 	def self.find(id, db)
-		db.execute("SELECT * FROM pokemon WHERE id=#{id}").flatten
+		db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
 	end
 	
 end
