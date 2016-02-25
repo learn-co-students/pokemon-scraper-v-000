bf63a37f27baef9012c075c929d9982fe03cd568
fiveinfinity <jlachance1@gmail.com>
2016-02-25 14:12:17 -0600

    tests passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  @@all = Array.new
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", @id, name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon").flatten
+  end
 end
