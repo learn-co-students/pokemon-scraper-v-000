c325b31a2b5fbc9cae7c0869749592a690d471d6
the-widget <brennenawana108ny@gmail.com>
2016-03-07 15:16:45 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);")
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", [name, type])
+  end
+
+  def self.find(id, database_connection)
+    database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).first
+  end
+
 end
