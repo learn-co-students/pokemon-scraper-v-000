ac64924027a6bb7f416e9a76069b7a9a0dc540cd
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-03-15 23:08:02 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+
+    #database_connection = SQLite3::Database.new(db)
+  end
+
+  def self.save(poke_name, poke_type, db)
+    pokemons = SQLite3::Database.new(db)
+#    db.execute("CREATE TABLE IF NOT EXISTS pokemons (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, type TEXT)")
+    db.execute("INSERT INTO pokemons (name, type) VALUES (?,?)",poke_name,poke_type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemons GROUP BY #{id}").flatten
+  end
 end
