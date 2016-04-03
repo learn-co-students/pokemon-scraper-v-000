d8d425da2b34ac146e79adcc20dcccc1eec2c7ab
abadfish <kirsten.ofarrell@gmail.com>
2016-04-03 18:24:30 -0400

    Done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  # @@all = []
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = SQLite3::Database.new('db/pokemon.db')
+    # @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type)
+    VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE id = 1;")
+  end
+
 end
