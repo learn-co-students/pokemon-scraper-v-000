fcc50e094f6985245d269415d1f227669ee1b734
motti770 <mottigottlieb@gmail.com>
2016-05-05 18:51:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id,name,type,db)
+    @id= id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, action, db)
+     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, action)
+  end
+
+  def self.find(name,db)
+      db.execute("SELECT * FROM pokemon WHERE name = name")
+  end
+
+
+#   describe ".find" do
+#     it 'finds a pokemon from the database' do
+#       newer_pokemon = Pokemon.save("Pikachu", "fire", @db)
+
+#       pikachu_from_db = Pokemon.find(1, @db)
+#       expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
+#     end
+#   end
+
+#   describe ".save" do
+#     it 'saves an instance of a pokemon with the correct id' do
+#       new_pokemon = Pokemon.save("Pikachu", "fire", @db)
+
+#       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+#       expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
+#     end
+#   end
+
 end
