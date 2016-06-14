6383fdf8ff48e8d8032d3694c13a7fe00d57c646
reichardn <reichardn@gmail.com>
2016-06-13 22:22:06 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name 
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id, db)
+    q = "
+    SELECT * FROM pokemon WHERE id = ?
+    "
+    db.execute(q, id)
+  end
+
 end
