54764fb13a8d4807febbcfb7e3cdd31c69d99099
rnhutsko <rnhutsko@gmail.com>
2016-06-15 14:37:46 -0400

    Completed RSPEC Tests for Pokemon class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,26 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initalize(id, name, type, db)
+  def initialize(id = nil, name, type, db)
     @id = id
     @name = name
     @type = type
     @db = db
   end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",[name,type])
+  end
+
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+
+  def self.find (id, db)
+    db.execute("SELECT Pokemon.id, Pokemon.name, Pokemon.type FROM pokemon WHERE Pokemon.id = ?", id)
+
+  end
+
 end
