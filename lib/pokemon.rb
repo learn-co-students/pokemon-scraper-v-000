09bfac8917a69746c1099b7584cf68bbe790ee28
Jonathan Stone <jon_p_stone@yahoo.com>
2016-06-17 06:09:24 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  @@all = []
+
+  def initialize(name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(id, name, type, database_connection)
+    database_connection.execute("INSERT INTO db (id, name, type) VALUES (?, ?, ?)", id, name, type)
+  end
+
+  def self.find(name)
+    self.all.detect{|s| s.name == name}
+  end
+
 end
