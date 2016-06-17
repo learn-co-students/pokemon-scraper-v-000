c28400991ea0d00cecf6d48d45c121260f0e9040
jd2rogers2 <jd2rogers2@gmail.com>
2016-06-17 12:17:05 -0400

    donzo

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id, name, type, database)
+    @id = id
+    @name = name
+    @type = type
+    @db = database
+  end
+
+  def self.save(pika, chu, database)
+    database.execute("INSERT INTO pokemon (name, type) VALUES ('#{pika}', '#{chu}');")
+  end
+
+  def self.find(id, database)
+    database.execute("SELECT * FROM pokemon;")
+  end
+
 end
