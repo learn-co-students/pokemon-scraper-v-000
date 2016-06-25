2ddf7d06fd3df1d3ba07a4c16c548f4af0a14250
Ivan Pilot <ivanpilot@gmail.com>
2016-06-25 22:11:43 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,12 @@
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
-
-  @@all = []
+  attr_accessor :id, :name, :type, :hp, :db
 
   def initialize(id, name, type, db)
     @id = id
     @name = name
     @type = type
     @db = db
-    @@all << self
   end
 
   def self.all
@@ -21,7 +18,11 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon")
+    db.execute("SELECT * FROM pokemon WHERE id = ?", id).first
+  end
+
+  def alter_hp(new_hp)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
 end
