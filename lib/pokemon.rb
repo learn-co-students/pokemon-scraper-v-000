179004576e42a2ec5ed2afc7630805da7f4a1470
Bob Habig <rfhab2@gmail.com>
2016-07-08 18:48:46 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :name, :id, :type, :db, :hp
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+
+  def self.find(id, db)
+    poke = db.execute("SELECT * FROM pokemon WHERE id=? LIMIT 1", id)
+  end
+
+
 end
