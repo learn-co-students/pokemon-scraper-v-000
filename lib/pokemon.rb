3ee73edb3d54d411d66b48c9ec1ce78b3bc444a4
William Jones <enquiries@forestfed.com>
2016-07-21 16:02:44 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,27 @@
 class Pokemon
 
-  def self.new(id, name, type, database_connection)
-    database_connection.execute("DROP TABLE IF EXISTS pokemon")
-    database_connection.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
+  @@all = []
+
+  def initialize(id, name, type, database_connection)
+    @id = id
+    @name = name
+    @type = type
+    @db = database_connection
+    @@all << self
+#    database_connection.execute("DROP TABLE IF EXISTS pokemon")
+#    database_connection.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
+  end
+
+  def self.all
+    @@all
   end
 
   def self.save(name, type, database_connection)
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id, database_connection)
+    database_connection.execute("SELECT * FROM pokemon WHERE id = 1;")
   end
 
 end
