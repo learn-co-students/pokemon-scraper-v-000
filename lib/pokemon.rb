0e01c0e2e7f84642032f2a088726c3436505f5a3
William Jones <enquiries@forestfed.com>
2016-07-21 14:33:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+
+  def self.new(id, name, type, database_connection)
+    database_connection.execute("DROP TABLE IF EXISTS pokemon")
+    database_connection.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
 end
