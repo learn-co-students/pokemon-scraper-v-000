112598ec61ff87b257c829b6c869f5b9586623e9
yeungn <nin.yeung@gmail.com>
2016-07-24 18:35:26 -0400

    bonus test error

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id, name, type, db)
+    @name = name
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon")
+  end
+
+  # def alter_hp(new_hp)
+    # db.execute("UPDATE pokemon SET hp = new_hp")
+  # end
 end
