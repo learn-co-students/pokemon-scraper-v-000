12522786fd2fcb14ad6baf58c2ca39486900252c
arturusfury <bri.reynolds@gmail.com>
2016-07-25 16:34:10 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id, name, type, db, hp = 60)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+  end
+
+  def self.save(pokemon, type, db)
+    db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', pokemon, type)
+  end
+
+  def self.find(poke_id, db)
+    pokemon_info = db.execute('SELECT * FROM pokemon WHERE id = ?', poke_id).first
+    new_id, new_name, new_type, new_hp = *pokemon_info
+    Pokemon.new(new_id, new_name, new_type, db, new_hp)
+  end
+
+  def alter_hp(new_hp)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?', new_hp, id)
+  end
 end
--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -3,37 +3,45 @@ require_relative "spec_helper"
 describe "Pokemon" do
   before do
     @db = SQLite3::Database.new(':memory:')
-    @db.execute("DROP TABLE IF EXISTS pokemon")
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
+    scraper = Scraper.new(@db)
+    scraper.scrape
   end
 
-  let(:pokemon) {Pokemon.new(1, "Pikachu", "fire", @db)}
+  describe "has caught all 151 from scraping" do
+    it "has all 151 pokemon" do
+      expect(@db.execute("SELECT COUNT(*) FROM pokemon").flatten.first).to eq(151)
+    end
 
-  describe ".initialize" do
-    it 'is initialized with a name, type and db' do
-      expect(pokemon).to respond_to(:id)
-      expect(pokemon).to respond_to(:name)
-      expect(pokemon).to respond_to(:type)
-      expect(pokemon).to respond_to(:db)
+    it "knows all the information about Horsea" do
+      expect(@db.execute("SELECT * FROM pokemon WHERE name = 'Horsea'").flatten).to eq([116, "Horsea", "Water"])
+    end
+
+    it "knows Psyduck is the 54th pokemon" do
+      expect(@db.execute("SELECT id FROM pokemon WHERE name = 'Psyduck'").flatten.first).to eq(54)
+    end
+
+    it "knows pokemon with the id 143 is Snorlax" do
+      expect(@db.execute("SELECT name FROM pokemon WHERE id = 143").flatten.first).to eq("Snorlax")
+    end
+
+    it "knows Charmander's type is fire" do
+      expect(@db.execute("SELECT type FROM pokemon WHERE name = 'Charmander'").flatten.first).to eq("Fire")
     end
   end
 
-  describe ".save" do
-    it 'saves an instance of a pokemon with the correct id' do
-      Pokemon.save("Pikachu", "fire", @db)
-
-      pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
-      expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
+  describe "saving more pokemon to the database" do
+    before do
+      Pokemon.save("Togepi", "Fairy", @db)
     end
-  end
 
-  describe ".find" do
-    it 'finds a pokemon from the database' do
-      Pokemon.save("Pikachu", "fire", @db)
+    it "knows the pokemon count increases" do
+      expect(@db.execute("SELECT COUNT(*) FROM pokemon").flatten.first).to eq(152)
+    end
 
-      pikachu_from_db = Pokemon.find(1, @db)
-      expect(pikachu_from_db).to eq([[1, "Pikachu", "fire"]])
+    it "has Togepi as the last pokemon" do
+      expect(@db.execute("SELECT name FROM pokemon ORDER BY id DESC LIMIT 1").flatten.first).to eq("Togepi")
     end
   end
 
@@ -47,25 +55,23 @@ describe "Pokemon" do
     end
 
     it "knows that a pokemon have a default hp of 60" do
-      pending "Implement the bonus section of the README. Then remove this line."
-      expect(@db.execute("knows_default_hp").flatten.first).to eq(60)
+      expect(@db.execute("SELECT hp FROM pokemon WHERE id = 1").flatten.first).to eq(60)
     end
 
     # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
     # He used splash. It wasn't very effective. It did one damage.
-    it "alters Pikachu's hp to 59" do
-      pending "Implement the bonus section of the README. Then remove this line."
-
+    it "alters Pickachu's hp to 59" do
       pikachu.alter_hp(59)
-      expect(@db.execute("alter_hp").flatten.first).to eq(59)
+      expect(@db.execute("SELECT hp FROM pokemon WHERE id = 25").flatten.first).to eq(59)
     end
 
     # Now we alter Magikarp's hp
     it "alters Magikarp's hp" do
-      pending "Implement the bonus section of the README. Then remove this line."
-
       magikarp.alter_hp(0)
-      expect(@db.execute("alter_hp").flatten.first).to eq(0)
+      expect(@db.execute("SELECT hp FROM pokemon WHERE id = 129").flatten.first).to eq(0)
     end
+
+    # The pokemon battle has now been won, and you are the Pokemon and SQL Master!
+
   end
 end
