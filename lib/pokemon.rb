4390131f99e741a0610afbeb188ab003ceed98bd
nicolefederici <nicolefederici@yahoo.com>
2016-08-05 18:22:52 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,12 +22,10 @@ attr_accessor :id, :name, :type, :db, :hp
     @@all
   end
 
-  def alter_hp(hp)
-    @hp = hp
-    db.execute("INSERT INTO pokemon (hp) VALUES (?)",hp)
-
+  def alter_hp(new_hp)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, id)
   end
- 
+
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
