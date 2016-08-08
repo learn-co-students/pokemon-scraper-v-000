a0b009586dce8f959f854f7d4e879df6a446ddd5
bmolina-nyc <bruce.d.molina@gmail.com>
2016-08-07 21:38:24 -0400

    Done.

--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -11,4 +11,9 @@ class SQLRunner
   def execute_sql(sql)
      sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
   end
+
+  def execute_create_hp_column
+
+  end
+
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db 
+
+  def initialize(id, name, type, db)
+    @id, @name, @type, @db = id, name, type, db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT id, name, type FROM pokemon")
+  end
+
 end
+
+
+
+
+# Pokemon
+#   .initialize
+#     is initialized with a name, type and db (FAILED - 1)
+#   .save
+#     saves an instance of a pokemon with the correct id (FAILED - 2)
+#   .find
+#     finds a pokemon from the database (FAILED - 3)
+#   BONUS
+#     knows that a pokemon have a default hp of 60 (FAILED - 4)
+#     alters Pikachu's hp to 59 (FAILED - 5)
+#     alters Magikarp's hp (FAILED - 6)
