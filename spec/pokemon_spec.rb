541adb61b23d524ff55460d03bae2e638d1e6f95
tylertaylor <vikingtyty@gmail.com>
2016-08-20 11:21:22 -0500

    Done.

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -47,7 +47,6 @@ describe "Pokemon" do
     end
 
     it "knows that a pokemon have a default hp of 60" do
-      pending "Implement the bonus section of the README. Then remove this line."
       expect(@db.execute("knows_default_hp").flatten.first).to eq(60)
     end
 
