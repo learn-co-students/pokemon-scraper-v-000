f7f436ecaeecefaef88c6296ec63d1246942d09d
Perla Yates <pianistps@yahoo.com>
2016-08-24 05:40:29 +0000

    finished main part of lab

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id, name, type, db, hp = 60)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp  = hp
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name,type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
+  end
+
 end
