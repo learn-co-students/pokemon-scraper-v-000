fc4594cb6d042aa3d81ad28a52406907fc63594d
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-05 17:49:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+  @@all = []
+
+  @db = SQLite3::Database.new('db/pokemon.db')
+  @db.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    insert = db.prepare("INSERT INTO pokemon(name, type) VALUES(?, ?, ?)")
+    db.execute(insert.execute(name, type))
+  end
+
 end
