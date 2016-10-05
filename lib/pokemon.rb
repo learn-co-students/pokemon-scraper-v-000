01a6ce92419dfb826a3bba3bdc74f4b3879ea440
Will Lynch <will.lynch91@gmail.com>
2016-10-05 22:26:02 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,10 +4,10 @@ class Pokemon
 
     @@all = []
 
-    def initialize(id: id, name: name, type: type, db: db)
+    def initialize(id: id, name: name, type: type, db: db, hp: hp = 60)
         @id = id
         @db = db
-        @hp = db.execute("SELECT hp FROM pokemon WHERE id = (?)", id).flatten.join.to_i
+        @hp = hp
         @name = name
         @type = type
         @@all << self
@@ -19,7 +19,7 @@ class Pokemon
 
     def self.find(id, db)
          row = db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten
-         self.new(id: row[0], name: row[1], type: row[2], db: db)
+         self.new(id: row[0], name: row[1], type: row[2], db: db, hp: row[3])
     end
 
     def alter_hp(hp, db)
