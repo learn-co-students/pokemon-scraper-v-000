523f3500259f0783c20eccff6d1aee8dc4a51b76
Emmanuel Owusu <e_owusu@live.com>
2016-10-08 16:35:16 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,7 @@
 class Pokemon
-
   attr_accessor :name, :type, :db, :id
 
-  def initialize(id = nil, name, type, db)
+  def initialize(id, name, type, db)
     @id = id
     @name = name
     @type = type
@@ -10,13 +9,17 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
   end
 
   def self.find(name, db)
     db.execute("SELECT * FROM pokemon")
   end
 
-  
+  def alter_hp(new_hp)
+    db.execute("UPDATE pokemon SET hp=new_hp")
+  end
+
+
 
 end
