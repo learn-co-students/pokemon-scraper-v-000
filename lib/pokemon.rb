9974c6879f8006e649db0713248639e285421487
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-10-19 11:32:55 -0400

    health power tracking added, all bonus tests passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,12 +13,16 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
   def self.find(id, db)
-    pk = db.execute("SELECT * FROM pokemon WHERE (?) = id",[id])
-    Pokemon.new(id: pk[0][0], name: pk[0][1], type: pk[0][2], db: db, hp:60)
+    pk = db.execute("SELECT * FROM pokemon WHERE (?) = id", id)
+    Pokemon.new(id: pk[0][0], name: pk[0][1], type: pk[0][2], hp: pk[0][3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
   def self.all
