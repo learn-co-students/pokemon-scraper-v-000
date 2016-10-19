c597ca09b7976c525b66e0b42db6371fbdfeb8ac
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-10-19 10:54:49 -0400

    all basic tests passing, beginning bonus work

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  @@all = []
+
+  def initialize(id: id, name: name, type: type, db: db, hp: nil)
+    self.id = id
+    self.name = name
+    self.type = type
+    self.db = db
+    self.hp = hp
+    Pokemon.all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
+  end
+
+  def self.find(id, db)
+    pk = db.execute("SELECT * FROM pokemon WHERE (?) = id",[id])
+    Pokemon.new(id: pk[0][0], name: pk[0][1], type: pk[0][2], db: db, hp:60)
+  end
+
+  def self.all
+    @@all
+  end
 end
