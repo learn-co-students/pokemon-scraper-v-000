203102444fe1d559478dc6d7f0e9bc27006bf0ca
Janine Rosen <janinemrosen@gmail.com>
2016-10-28 17:50:31 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,7 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def intialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:)
     @id = id,
     @name = name,
     @type = type,
