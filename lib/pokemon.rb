975fc21682934309860f5272145d606496324577
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-11-02 03:36:36 +0000

    completed class and instance methods in pokemon.rb, added alter_table_migration.db

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db, :hp
+
+  def initialize(attributes = {:hp => nil})
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @type = attributes[:type]
+    @db = attributes[:db]
+    @hp = attributes[:hp]
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?;', [new_hp, @id])
+  end
+
+  def self.find(id, db)
+    poke_arr = db.execute('SELECT * FROM pokemon WHERE id=?', id)[0]
+    self.new({
+      :id => poke_arr[0],
+      :name => poke_arr[1],
+      :type => poke_arr[2],
+      :hp => poke_arr[3],
+      :db => db
+    })
+  end
+
+  def self.save(pk_name, pk_type, db)
+    db.query('INSERT INTO pokemon (name, type) VALUES (?,?)', [pk_name, pk_type])
+  end
+
 end
