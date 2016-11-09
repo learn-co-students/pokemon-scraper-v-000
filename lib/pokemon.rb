c03c7a8ac6b0be0d04ae8ff5a7d037e2484c66b2
William Story <wstory@villanova.edu>
2016-11-09 00:43:51 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  @@all = []
+
+  def initialize(hash)
+    hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id, db)
+    @@all.find {|pokemon| pokemon.id == id}
+  end
+
 end
