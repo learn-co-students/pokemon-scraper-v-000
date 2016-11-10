0a5d823bd0d5e3283eaa4188d40db1c67b0a36fe
bharti gurnaney <bharti0717@yahoo.com>
2016-11-10 17:50:42 +0000

    Done.

---
+++ db/create_hp_column.sql
@@ -0,0 +1,2 @@
+ALTER TABLE pokemon add (hp INTEGER )
+update table pokemon set hp = 60
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,13 @@
 require "pry"
 class Pokemon
-  attr_accessor :name,:type,:db, :id
+  attr_accessor :name,:type,:db, :id,:hp
   def initialize(pokemon_hash)
     @id=pokemon_hash[:id]
     @name=pokemon_hash[:name]
     @type=pokemon_hash[:type]
     @db=pokemon_hash[:db]
+    @hp = pokemon_hash[:hp]
+
   end
 
   def self.save(name, type, database_connection)
@@ -15,11 +17,16 @@ class Pokemon
   def self.find(id, database_connection)
     pokemon_hash={}
     pokemon = database_connection.execute("SELECT * FROM POKEMON where id = ?", id.to_i)
-    binding.pry
     pokemon_hash[:id]=pokemon[0][0]
     pokemon_hash[:name]=pokemon[0][1]
     pokemon_hash[:type]=pokemon[0][2]
+    pokemon_hash[:hp]=pokemon[0][3]
     pokemon_hash[:db]=database_connection
     Pokemon.new(pokemon_hash)
   end
+
+  def alter_hp(hp,database_connection)
+  database_connection.execute("update table pokemon set hp = ? where id = ?", hp, self.id)
+  end
+
 end
