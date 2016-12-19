fcc180955807ae79d9b25c89f0a996d9af50ef6e
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-19 18:14:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,6 +15,6 @@ class Pokemon
    end
 
    def self.find(id, db)
-     db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+     db.execute("SELECT * FROM pokemon WHERE id=?", id)
    end
 end
