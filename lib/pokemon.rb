e374e5873ccfa22bf826c5fe096527f2b391c87a
HANA LEANDER <hana0070@gmail.com>
2016-12-20 23:24:47 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
 class Pokemon
+
+  def initialize(name:, type:, db:)
+
+    @name = name
+    @type = type
+    @db = db
+
+    binding.pry
+  end
+
+  def self.save(id, name, type)
+    @db.excute("INSERT INTO pokemon (id, name, type) VALUE (?, ?, ?)", id, name, type)
+  end
+
+
 end
