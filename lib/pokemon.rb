35b398b3998c985136a6a056c43eb01ac67f459d
Mary Umoh <umohm12@gmail.com>
2016-12-22 22:15:19 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id = nil, name, type, db)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db, id)
+        @db.execute("INSERT INTO pokemon (name, type, db, id)
+        VALUES (?, ?, ?)", name, type, db, id)
+    end
+
+
 end
