706bc043cc2ccc33863aff08beec2a0fbb1641da
Mary Umoh <umohm12@gmail.com>
2016-12-22 23:48:02 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Pokemon
-    attr_accessor :id, :name, :type, :db, :hp
+    attr_accessor :id, :name, :type, :db
 
-    def initialize(id:, name:, type:, db:, hp:)
+    def initialize(id:, name:, type:, db:)
         @id = id
         @name = name
         @type = type
