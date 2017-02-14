46ac15d7e60e9663a248717e6b1f77276ca2a106
Alex Glover <alexanderbglover@gmail.com>
2017-02-14 19:53:08 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 @@all = []
 
-class Pokemon
-  attr_accessor :id, :name, :type, :db
+DEFAULT_HP = 60
 
-  def initialize(pokemon)
+class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(pokemon, hp=nil)
     @id = pokemon[:id]
     @name = pokemon[:name]
     @type = pokemon[:type]
     @db = pokemon[:db]
+    @hp = pokemon[:hp]
     @@all << self
   end
 
   def self.save(name, type, database_connection)
-    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+    begin
+      database_connection.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?);", name, type, DEFAULT_HP)
+    rescue
+      database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+    end
   end
 
   def self.find(id, database_connection)
@@ -22,9 +29,14 @@ class Pokemon
     pokemon_obj = {
       id: array[0],
       name: array[1],
-      type: array[2]
+      type: array[2],
+      hp: array[3] || DEFAULT_HP
     }
     self.new(pokemon_obj)
+  end
+
+  def alter_hp(new_hp, database_connection)
+    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
 end
