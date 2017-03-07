605578036d50da94a48495f7e5f18b8e704b5550
bentonwong <bentonwong@gmail.com>
2017-03-06 23:46:47 -0800

    added initialize, save, and find; attempted bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,15 +2,24 @@ require 'pry'
 
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id: 1, name:, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
   end
 
-end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
 
-Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+  def self.find(id, db)
+    name = db.execute("SELECT pokemon.name FROM pokemon WHERE id = id;").join(',')
+    type = db.execute("SELECT pokemon.type FROM pokemon WHERE id = id;").join(',')
+    #hp = db.execute("SELECT pokemon.hp FROM pokemon WHERE id = id;").join(',')
+    Pokemon.new(id: id, name: name, type: type, db: db)
+  end
+
+end
