d6d37185760cb17dfd169760f2226f007a0b2e57
bentonwong <bentonwong@gmail.com>
2017-03-06 23:02:22 -0800

    finished initialize

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
+require 'pry'
+
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
 end
+
+Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
