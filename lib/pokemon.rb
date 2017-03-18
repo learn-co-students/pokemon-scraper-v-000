952af8a0a7c4d8536bb97025375e49fc062a4cdb
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-03-18 20:38:42 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,31 +1,22 @@
 require 'pry'
 class Pokemon
-attr_accessor :id, :name, :type, :db
+attr_accessor :id, :name, :type, :db, :hp
 
-def initialize(obj)
-  @id = obj[:id]
-  @name = obj[:name]
-  @type = obj[:type]
-  @db = obj[:db]
+    def initialize(id:, name:, type:, hp:nil, db:)
+      @id, @name, @type, @db, @hp = id, name, type, db, hp
+    end
 
-end
+    def self.save(name,type,db)
+       db.execute("INSERT INTO pokemon(name,type) VALUES(?,?)",name,type )
+    end
 
-def self.save(name,type,db)
-   db.execute("INSERT INTO pokemon(name,type) VALUES(?,?)",name,type )
-end
+    def self.find(p_id,db)
+      result_array = db.execute("SELECT * FROM pokemon WHERE id = ?" ,p_id).first
+      self.new(id: result_array[0], name: result_array[1], type: result_array[2], hp: result_array[3], db: db)
+    end
 
-def self.find(p_id,db)
-result_array = db.execute("SELECT * FROM pokemon WHERE id = ?" ,p_id)
-  obj = {}
-  obj[:id] = result_array[0][0]
- obj[:name] = result_array[0][1]
- obj[:type] = result_array[0][2]
- self.new(obj)
-end
-
-def alter_hp(hp,db)
- db.execute("ALTER TABLE pokemon ADD COLUMN hp int")
- db.execute("UPDATE pokemon SET hp = ?",60)
-end
+    def alter_hp(hp,db)
+      db.execute("UPDATE pokemon SET hp = ? WHERE  id = ?",hp, self.id)
+    end
 
 end
