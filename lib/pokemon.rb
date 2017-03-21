2a02d7e22c7b33d38e37673350d02474d5faa5cd
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-21 21:11:49 +0000

    I

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 class Pokemon
+
+attr_accessor :db, :id, :type, :name, :hp
+
+  def initialize(db, id = "", type = "", name = "") #, hp = nil) #tests were very unclear. Needed these stupid arguments, not just db.
+    #@hp = hp
+    @db = db
+    @id = id
+    @type = type
+    @name = name
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type);
+  end
+
+  def self.find(id, db)
+    pok = db.execute("SELECT * from pokemon where id = id"); #binding.pry
+    Pokemon.new(@db = db, id = pok[0][0], type = pok[0][2], name = pok[0][1])#, hp = pok[0][3]) #apparently the order mattered -_-
+  end
 end
