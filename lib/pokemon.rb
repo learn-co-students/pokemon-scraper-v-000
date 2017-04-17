0f936bfeedde08bf6785e070aacf8404576f5a04
Sergio Mendoza <smendoza787@gmail.com>
2017-04-17 22:48:56 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,15 +12,15 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])
+    db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", name, type)
   end
 
-  def self.find(id, db)
-    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = (?)", [id]).flatten
+  def self.find(id_num, db)
+    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = (?)", id_num).flatten
     self.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], hp: new_pokemon[3], db: db)
   end
 
-  def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp = (?) WHERE id = (?)", [hp, self.id])
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 end
