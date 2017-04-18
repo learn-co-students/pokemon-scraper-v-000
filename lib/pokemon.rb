91c870537d2bc41c813e17d95bee15d78d9df3ab
ETphonehomeslice <mk.etlinger@gmail.com>
2017-04-18 19:05:33 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ require 'sqlite3'
 
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
   @@all = []
 
@@ -12,12 +12,13 @@ class Pokemon
     @name = db[:name]
     @type = db[:type]
     @db = db
+    @hp = db[:hp] unless db[:hp].nil?
     @@all << self
   end
 
-  def self.save(name, type, db_connection)
+  def self.save(name, type, hp = nil, db_connection)
     db_connection.execute("INSERT INTO pokemon (name, type)
-    VALUES (?, ? )", [name, type])
+    VALUES (?, ?)", name, type)
   end
 
   def self.all
@@ -29,7 +30,13 @@ class Pokemon
       FROM pokemon
       WHERE pokemon.id = ?", id).flatten
 
-    Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: db)
+    Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], hp: new_pokemon[3], db: db)
+  end
+
+  def alter_hp(remaining_hp, db)
+    db.execute("UPDATE pokemon 
+    SET hp = (?) 
+    WHERE name = (?);", remaining_hp, self.name)
   end
 
 
