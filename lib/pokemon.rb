d82992da70c3c04b574e5a222679a51839fe7849
John Ziegner <jziegner23@gmail.com>
2017-05-22 20:19:33 +0000

    Done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
+
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id, @name, @type, @hp = id, name, type, hp
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute( "INSERT INTO Pokemon (name, type) VALUES ( ?, ? )", name, type)
+  end
+
+  def self.find(id, db)
+    new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], hp: new_pokemon[3], db: db)
+  end
+
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  end
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,69 @@
-class Scraper
-  attr_accessor :file, :parsed_file, :all_pokemon, :db
 
-  def initialize(db)
-    self.db = db
-    self.file = File.open("pokemon_index.html")
-    self.parsed_file = Nokogiri::HTML.parse(file)
-    self.all_pokemon = catch_em_all
+describe "Pokemon" do
+  before do
+    @db = SQLite3::Database.new(':memory:')
+    @db.execute("DROP TABLE IF EXISTS pokemon")
+    @sql_runner = SQLRunner.new(@db)
+    @sql_runner.execute_schema_migration_sql
   end
 
-  def catch_em_all
-    self.all_pokemon = parsed_file.css(".infocard-tall")
-  end
+  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
 
-  def get_pokemon_name_from(node)
-    node.css(".ent-name").text
-  end
-
-  def get_pokemon_type_from(node)
-    node.css(".itype").text
-  end
-
-  def scrape
-    all_pokemon.each do |pk_node|
-      pk_name = get_pokemon_name_from(pk_node)
-      pk_type = get_pokemon_type_from(pk_node)
-      Pokemon.save(pk_name, pk_type, db)
+  describe ".initialize" do
+    it 'is initialized with keyword arguments of name, type and db' do
+      expect(pokemon).to respond_to(:id)
+      expect(pokemon).to respond_to(:name)
+      expect(pokemon).to respond_to(:type)
+      expect(pokemon).to respond_to(:db)
     end
   end
 
+  describe ".save" do
+    it 'saves an instance of a pokemon with the correct id' do
+      Pokemon.save("Pikachu", "electric", @db)
+
+      pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+    end
+  end
+
+  describe ".find" do
+    it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
+
+      Pokemon.save("Pikachu", "electric", @db)
+
+      pikachu_from_db = Pokemon.find(1, @db)
+      expect(pikachu_from_db.id).to eq(1)
+      expect(pikachu_from_db.name).to eq("Pikachu")
+      expect(pikachu_from_db.type).to eq("electric")
+    end
+  end
+
+  describe "BONUS" do
+
+    before do
+      @sql_runner.execute_create_hp_column
+      Pokemon.save('Pikachu', 'electric', @db)
+      Pokemon.save('Magikarp', 'water', @db)
+    end
+
+    let(:pikachu){Pokemon.find(1, @db)}
+    let(:magikarp){Pokemon.find(2, @db)}
+
+
+    it "knows that a pokemon have a default hp of 60" do
+      expect(@db.execute("SELECT hp FROM pokemon").flatten.first).to eq(60)
+    end
+
+
+    it "alters Pikachu's hp to 59" do
+      pikachu.alter_hp(59, @db)
+      expect(Pokemon.find(1, @db).hp).to eq(59)
+    end
+    
+    it "alters Magikarp's hp" do
+      magikarp.alter_hp(0, @db)
+      expect(Pokemon.find(2, @db).hp).to eq(0)
+    end
+  end
 end
