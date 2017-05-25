19ef6283a79e8bf18ce0f1f3178101ac0300ed6e
Brad Watson <brad.watson.orlando@gmail.com>
2017-05-25 09:13:02 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,6 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
-  def initialize(id:, name:, type:, hp: nil, db:)
-    @id, @name, @type, @hp, @db = id, name, type, hp, db
-  end
-
-
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
@@ -13,6 +8,10 @@ class Pokemon
   def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
+
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id, @name, @type, @hp, @db = id, name, type, hp, db
   end
 
   def alter_hp(new_hp, db)
