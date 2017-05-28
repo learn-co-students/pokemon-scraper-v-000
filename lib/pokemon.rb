e8665916ad0262abedd4c3e75aeb5d8a57ecb0ef
Spencer Evans <spencevans@gmail.com>
2017-05-28 19:51:42 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,14 @@
 require 'pry'
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
   def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = nil
   end
 
   def self.save(name, type, database_connection)
@@ -17,6 +18,11 @@ class Pokemon
   def self.find(id, database_connection)
     pokemon_info = database_connection.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
     Pokemon.new({id: pokemon_info[0], name: pokemon_info[1],type: pokemon_info[2], db: pokemon_info[3]})
+  end
+
+  def alter_hp(hp, database_connection)
+    binding.pry 
+    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?",hp, id)
   end
 
 end
