12c7c442fde511b95bbdc4233a14330ed3f81c0a
Will-Sauer <will@heueso.com>
2017-06-02 17:05:49 -0700

    first two pokemon specs passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,45 @@
 class Pokemon
+
+#----------------------------------------------------------------------------------------
+#macros, vars, attrs
+attr_accessor :id, :name, :type, :db
+
+
+#----------------------------------------------------------------------------------------
+#instance
+def initialize (id:, name:,type:,db:)
+@id = id
+@name = name
+@type = type
+@db = db
+end
+
+#----------------------------------------------------------------------------------------
+#class
+def self.save (name, type, db)
+#one method (using the db object rather than statement object below)
+# db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?);", 1, name, type)
+
+#another method to save (using the statment object)
+statement = db.prepare("INSERT INTO pokemon (id, name, type) VALUES (?,?,?);")
+statement.bind_params(1,name,type)
+statement.execute
+end
+
+def self.find (id,db)
+db.results_as_hash = true
+
+result = db.execute("SELECT * FROM pokemon where id = ?;", id)
+
+new_poke = Pokemon.new(id: 9, name: "default", type: "defaulte", db: db)
+
+binding.pry
+result.each {|key,value| new_poke.instance_variable_set("@#{key}", "#{value}")}
+
+new_poke
+
+end
+
+
+#end of class
 end
