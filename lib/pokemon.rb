3673fa95d615c49a75b3fb58f94489ec1f789f96
Will-Sauer <will@heueso.com>
2017-06-03 09:58:50 -0700

    all required specs working; now doing bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,16 +27,34 @@ statement.execute
 end
 
 def self.find (id,db)
-db.results_as_hash = true
 
-result = db.execute("SELECT * FROM pokemon where id = ?;", id)
+#get the row back from the database
+statement = db.prepare("SELECT * FROM pokemon where id = ?;")
+statement.bind_params(id)
+result = statement.execute
 
-new_poke = Pokemon.new(id: 9, name: "default", type: "defaulte", db: db)
-
-binding.pry
-result.each {|key,value| new_poke.instance_variable_set("@#{key}", "#{value}")}
-
-new_poke
+#iterate through the result in such a way that:
+#1) multiple rows can be handled
+#2) that we don't rely on the order of the columns to set our isntance variables, we can isntead actually "read" them
+#   and be sure the right columns get to the right instance variables'
+poke_array = []
+result.each_hash {|this_hash| 
+                              new_poke = Pokemon.new(id: 9, name: "default", type: "defaulte", db: db)
+                              
+                              this_hash.each {|key,value|
+                                                        
+                                                        #if this is the id column, you store as an integer rather than 
+                                                        #an interpolated string
+                                                        if key != "id"
+                                                        new_poke.instance_variable_set("@#{key}", "#{value}")
+                                                        else
+                                                        new_poke.instance_variable_set("@#{key}", value)
+                                                        end
+                                            }
+                              poke_array << new_poke
+                 }
+#return the found results (in this case just one of them, but could be built to dynamically decide how many to return)
+poke_array[0]
 
 end
 
