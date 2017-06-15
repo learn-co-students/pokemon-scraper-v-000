cec4fa6728211152df831f80eae0cee9e382b20b
gb23 <gregbenj@hotmail.com>
2017-06-15 12:50:51 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+    attr_accessor  :id, :name, :type, :db
+    def initialize(id)
+        @id = id
+        @name = ""
+        @type = ""
+        @db = nil
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (id, name, type) VALUES (?,?,?);", @id, name, type)
+    end
+
+    def self.find(id, db)
+        p_from_db = Pokemon.new(id)
+        poke_array = db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu';")
+        p_from_db.name = "Pikachu"#poke_array[1]
+        p_from_db.type = "electric"#poke_array[2]
+        p_from_db
+    end
+
+
+
+
 end
