0e4dd985e543ff37c3396ff422782502b0f4ab1b
Lia HUANG <lisa.yc.huang@gmail.com>
2017-06-15 17:48:05 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,6 @@ class Pokemon
 
    def self.find(id, db)
      pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-     binding.pry
      Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
    end
 
