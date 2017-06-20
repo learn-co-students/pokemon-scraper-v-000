48f1b1324edb20d9123d4614b81398f5fbb88a47
hyoyou <heatherhyou@gmail.com>
2017-06-20 14:38:44 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,5 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :hp, :db
-  @@all = []
+  attr_accessor :id, :name, :type, :db, :hp
 
   def initialize(id:, name:, type:, hp: nil, db:)
     @id = id
@@ -8,11 +7,6 @@ class Pokemon
     @type = type
     @hp = hp
     @db = db
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def self.save(name, type, db)
@@ -20,8 +14,8 @@ class Pokemon
   end
 
   def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).flatten
-    self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
 
   def alter_hp(new_hp, db)
