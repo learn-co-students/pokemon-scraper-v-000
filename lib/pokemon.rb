523745caf1d40b2f6b467cc923bdbfd50712e844
Leonardo Guzman <lguzman@me.com>
2017-07-10 19:09:23 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,13 +3,14 @@ class Pokemon
 #since we are creatind the DB with classes we need to set these up as classes
 #must use attr_accessor as we will be reading and writing to and from the database
 # We need to add the 'id' as the databse will need the id to locate the items in the database
-attr_accessor :id, :name, :type, :db
+attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, hp: nil, db:)
     @name = name
     @type = type
     @db = db
     @id = id
+    @hp = hp
   end
 
   # Here we are saving the information to be placed into the database
@@ -20,7 +21,11 @@ attr_accessor :id, :name, :type, :db
 
   def self.find(id_num, db)
     info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
-    Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
+    Pokemon.new(id: info[0], name: info[1], type: info[2], hp: info[3], db: db)
+  end
+
+  def alter_hp(update_hp, db)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id = ?', update_hp, self.id)
   end
 
 end
