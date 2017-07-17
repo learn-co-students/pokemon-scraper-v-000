8c005cf6552fb1160843289fea11c226f3a1311b
Mason Ellwood <masonellwood@gmail.com>
2017-07-16 23:48:18 -0500

    passed lab working on bonus

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
+require "pry"
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: "default", name: "default", type: "default", db: "default")
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(id, db)
+    pokemon_find = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id)
+    new_pokemon = self.new(
+      id: pokemon_find[0][0],
+      name: "#{pokemon_find[0][1]}",
+      type: "#{pokemon_find[0][2]}"
+      )
+  end
 end
