93d7f1db6405f56fed5fa10fd431c8fc981a7969
Ron Morgan <ronm7581@gmail.com>
2017-07-23 23:22:53 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+db = SQLite3::Database.new('db/pokemon.db')
+
 class Pokemon
+
+  @@all = []
+
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(options={})
+    @id = options[:id]
+    @name = options[:name]
+    @type = options[:type]
+    @db = options[:db]
+    @hp = options[:hp]
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    name = db.execute("SELECT name FROM pokemon WHERE id = (?)", id).first.first
+    type = db.execute("SELECT type FROM pokemon WHERE id = (?)", id).first.first
+    hp = db.execute("SELECT hp FROM pokemon WHERE id = (?)", id).first.first
+    self.new({name: name, type: type, db: db, id: id, hp: hp})
+  end
+
+  def alter_hp(hp, db)
+    db.execute('UPDATE pokemon SET hp = ? WHERE id=?', hp, id)
+  end
+
 end
