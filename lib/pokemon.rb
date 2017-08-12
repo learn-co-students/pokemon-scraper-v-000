938fdca25ffd42d15341280074ceb6a540827db9
Jonathan <jonathanyi81@gmail.com>
2017-08-12 14:02:38 -0400

    its working now, something about he way initialize was assigning values was wrong. not clear on why this fixed it. will have to look more into keyword assignments

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 
   #https://robots.thoughtbot.com/ruby-2-keyword-arguments
   def initialize(id:, name:, type:, db:)
-    @id =id, @name = name, @type = type, @db = db
+    @id, @name, @type, @db = id, name, type, db
   end
 
   def self.save(name, type, db)
@@ -18,7 +18,8 @@ class Pokemon
 
   def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    #binding.pry
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
-  end
+  end # find
 
 end # Pokemon
