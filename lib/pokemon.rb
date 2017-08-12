9d42c0669107963b726a8f1240f5924e1d4fa2f5
Jonathan <jonathanyi81@gmail.com>
2017-08-12 12:59:50 -0400

    had a hard time figuring out what was need with keyword argument. save is complete will work on find

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
-end
+  attr_accessor :id, :name, :type, :db
+
+  #https://robots.thoughtbot.com/ruby-2-keyword-arguments
+  def initialize(id:, name:, type:, db:)
+    @id =id, @name = name, @type = type, @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
+  end # save
+
+  def self.find
+
+  end # find
+
+end # Pokemon
