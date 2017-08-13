ab476dd8880440c5a6b24b04d9896a43cc21c58a
Elvin <Dimix181@Gmail.com>
2017-08-13 15:16:11 -0400

    inserts a new column hp to Pokemon table with a default value of 60. TODO alter_hp method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,31 +1,34 @@
 
 class Pokemon
-attr_accessor :id, :name, :type, :db
+attr_accessor :id, :name, :type, :db, :hp
 
   def self.save (name, type, db)
     db.execute("INSERT INTO pokemon (name, type)
     VALUES (?, ? )", name, type)
-
   end
 
-  def self.find(id, db)
+  def self.find(id, db, hp=nil)
     #selects the pokemon from the table base on the id. results are in array form
   pokemon_array = db.execute("SELECT * FROM Pokemon WHERE id=?",id).flatten!
 
       #creates an instance by creating a hash and creating an instance
    new_pokemon = self.new({id:pokemon_array[0], name:pokemon_array[1],
-   type:pokemon_array[2],db:db})
-
-
- end
+   type:pokemon_array[2],
+   db:db,
+   hp:hp,})
+  end
 
   def initialize(hash)
     @id = hash[:id]
     @name = hash[:name]
     @type = hash[:type]
     @db = hash[:db]
+    @hp = hash[:hp]
   end
 
+  def alter_hp(hp,db)
+    binding.pry
+  end
 
 
 
