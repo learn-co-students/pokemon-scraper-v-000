a90bc2cc6e9c0ca36cc435f3d6de253a65089af9
github username <github email address>
2017-09-06 18:31:42 -0400

    2/3 tests passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,25 @@
 class Pokemon
-     "Writing a Blog post on how to use git. "
-end
+     attr_accessor :id, :name, :type, :db
+     @@all = []
+
+     def initialize(id:, name:, type:, db:)
+       @id = id
+       @name = name
+       @type = type
+       @db = db
+       @@all << self
+     end
+
+     def self.all
+       @@all
+     end
+
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
+    end
+
+    def self.find(id,db)
+
+    end
+
+   end
