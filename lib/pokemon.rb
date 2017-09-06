26ff45a365ba48eace4e0418f3c48b54d0ab2f77
Alexander Friedland <friedlandaj@gmail.com>
2017-09-06 04:23:46 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id)
+    @id = id
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES(?, ?)", name, type)
+  end
+=begin
+Pokemon.save("Pikachu", "electric", @db)
+
+pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+=end
 end
