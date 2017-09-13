bfb6b4779bfe17cad351fb62969f37e20b990dca
Alexandra <superbiscuit@gmail.com>
2017-09-13 13:20:20 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+	attr_accessor :id, :name, :type, :hp, :db
+
+	DATABASE_CONNECTION = SQLite3::Database.new('db/pokemon.db')
+
+	def initialize(attributes)
+		attributes.each {|attribute, value| self.send(("#{attribute}="), value)}
+	end
+
+	def self.save(name, type, db)
+		sql = File.read("db/schema_migration.sql")
+		db.execute_batch(sql)
+		db.execute("
+			INSERT INTO pokemon
+			(name, type)
+			VALUES (?,?)
+			",
+			name, type)
+	end
+
+	def self.find(id, db)
+		sql = File.read("db/schema_migration.sql")
+		db.execute_batch(sql)
+		attributes = db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten
+		Pokemon.new(id: attributes[0], name: attributes[1], type: attributes[2], hp: attributes[3])
+	end
+
+	def alter_hp(new_hp, db)
+		db.execute("UPDATE pokemon SET hp = #{new_hp} WHERE id =#{id}")
+	end
+
 end
