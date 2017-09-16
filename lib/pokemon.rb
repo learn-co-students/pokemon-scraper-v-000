5a3b3deb9c2e9c035904a9310aa990e3b2943f6a
Archana Burra <burra.archana@gmail.com>
2017-09-16 17:07:22 -0400

    finished project

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(characteristics, hp=nil)
+    @id = characteristics[:id]
+    @name = characteristics[:name]
+    @type = characteristics[:type]
+    @db = characteristics[:db]
+    @hp = characteristics[:hp]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+
+  def self.find(id, db)
+    char_array = db.execute("SELECT * FROM pokemon WHERE id = (?)", id)
+
+    characteristics = {
+      :id => char_array[0][0],
+      :name => char_array[0][1],
+      :type => char_array[0][2],
+      :hp => char_array[0][3],
+      :db => db
+    }
+
+    self.new(characteristics, 60)
+  end
+
+  def alter_hp(new_hp, db)
+    @hp = new_hp
+    db.execute("UPDATE pokemon SET hp = (?) WHERE id = (?)", new_hp, @id)
+  end
 end
