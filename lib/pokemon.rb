7eeafa2cf5c22892987b170f6167ad426ddc1ac0
Jameson Bass <jamesonbass@gmail.com>
2017-09-21 17:54:53 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,14 +9,14 @@ class Pokemon
 
   def self.save(name, type, db)
     #insert values into database
-    db.execute("INSERT INTO pokemon ( name, type ) VALUES ( ?, ?)", [name, type])
+    db.execute("INSERT INTO pokemon ( name, type ) VALUES (? , ?)", [name, type])
   end
 
   def self.find(id, db)
 
       pokemon_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
 
-      new_pokemon = self.new(:id => pokemon_db[0][0], :name => pokemon_db[0][1], :type => pokemon_db[0][2], :hp => pokemon_db[0][3])
+      new_pokemon = Pokemon.new(:id => pokemon_db[0][0], :name => pokemon_db[0][1], :type => pokemon_db[0][2], :hp => pokemon_db[0][3])
 
       new_pokemon
   end
