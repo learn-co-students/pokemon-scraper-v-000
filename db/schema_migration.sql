97f24d39314be767abe0bfbe379dbdca516f0a1c
Guesly  Germain <germain1211@outlook.com>
2017-10-03 13:43:50 +0000

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,1 +1,4 @@
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+-- ALTER TABLE pokemon
+-- ADD hp INTEGER
+-- CONSTRAINT DF_pokemon DEFAULT null
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+require 'pry'
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  @@all=[]
+
+  def initialize(db)
+    self.db = db
+    self.id = id
+    self.name = name
+    self.type = type
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", @id, name, type )
+  end
+
+  def self.find(id, db)
+    pokemon_info = db.execute("SELECT * from pokemon WHERE id = ?", id).flatten
+    self.new(db).tap do |pokemon|
+      pokemon.id = pokemon_info[0]
+      pokemon.name = pokemon_info[1]
+      pokemon.type = pokemon_info[2]
+      # binding.pry
+    end
+    end
+
 end
