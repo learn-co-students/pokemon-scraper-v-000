0485e7e44b97420bad2ba8f11d3c94b7c2ebae64
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-04 02:59:28 +0000

    passing 1 of 3 tests - self.save

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
-end
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(pk_name, pk_type, db)
+    db.prepare("INSERT INTO pokemon (name, type) VALUES (?, ?)" ).execute(pk_name, pk_type)
+  end
+
+  def self.find(id, db)
+    result = db.query("SELECT * FROM"  db "WHERE id = (?)", id)
+    binding.pry
+    Pokemon.new(id, result[1], result[2], db)
+  end
+
+end  #  End of Class
