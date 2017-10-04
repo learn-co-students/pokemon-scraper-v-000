f642afb4894233e202830b599c08023ca96e107c
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-04 17:16:32 +0000

    passing all tests

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,11 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id, name, type, db)
-    @id = id
+  def initialize(id: nil, name:, type:, db:)
     @name = name
     @type = type
     @db = db
+    @id = id
   end
 
   def self.save(pk_name, pk_type, db)
@@ -13,9 +13,8 @@ class Pokemon
   end
 
   def self.find(id, db)
-    result = db.query("SELECT * FROM"  db "WHERE id = (?)", id)
-    binding.pry
-    Pokemon.new(id, result[1], result[2], db)
+    result = db.query("SELECT * FROM pokemon WHERE id = (?)", id).first
+    Pokemon.new(id: id,name: result[1], type: result[2], db: db)
   end
 
 end  #  End of Class
