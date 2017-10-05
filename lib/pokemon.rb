c5201aa7abfb36b427e169f1554d6b15424ffa26
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-05 01:55:40 +0000

    create alter_hp method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db, :hp
+  attr_accessor :id, :name, :type, :db, :hp, :alter_hp
 
   def initialize(id: nil, name:, type:, db:, hp: nil)
     @name = name
@@ -9,13 +9,18 @@ class Pokemon
     @hp = hp
   end
 
+  def alter_hp(hp, db)
+    db.prepare("UPDATE pokemon SET hp = (?)" ).execute(hp)
+  end
+    
   def self.save(pk_name, pk_type, db)
     db.prepare("INSERT INTO pokemon (name, type) VALUES (?, ?)" ).execute(pk_name, pk_type)
+
   end
 
   def self.find(id, db)
     result = db.query("SELECT * FROM pokemon WHERE id = (?)", id).first
-    Pokemon.new(id: id,name: result[1], type: result[2], db: db, hp: 60)
+    Pokemon.new(id: id,name: result[1], type: result[2], db: db, hp: result[3])
   end
 
 end  #  End of Class
