d42c4cb3bf3fdde794f1811d1ad180fa9df87aec
github username <github email address>
2017-10-10 07:20:35 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,21 +5,21 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id=nil, pk_name=nil, pk_type=nil, db=nil)
+  def initialize(id=nil, pk_name=nil, pk_type=nil, db=nil, hp=nil)
     @id = id
     @name = pk_name
     @type = pk_type
     @da = db
   end
 
-  def self.save(pk_name, pk_type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", pk_name, pk_type)
+  def self.save(pk_name, pk_type, db, hp=60)
+    db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?)", pk_name, pk_type, hp)
   end
 
   def self.find(id, db)
     pokemon_name_by_id = db.execute("SELECT * FROM pokemon WHERE pokemon.id= ?", id).flatten
 
-    new_pokemon = Pokemon.new(pokemon_name_by_id[0], pokemon_name_by_id[1], pokemon_name_by_id[2])
+    new_pokemon = Pokemon.new(pokemon_name_by_id[0], pokemon_name_by_id[1], pokemon_name_by_id[2], pokemon_name_by_id[3])
 
   end
 end
