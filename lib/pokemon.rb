df9d3d06018968bd7467277348aa094c6f23a1a2
Sam Ojling <s.ojling@gmail.com>
2017-10-09 21:22:19 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
   end
 
   def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp = ?", [hp])
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [hp, self.id])
   end
 
 end
