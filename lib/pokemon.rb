3daaca34282c2f3e379854d87c22195b3dcb40bc
GusVilla303 <gusvilla303@gmail.com>
2017-10-10 11:37:35 -0600

    Add the .find method to Pokemon and pass the specs.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Pokemon
 
   attr_reader :id, :name, :type, :db
@@ -7,5 +9,14 @@ class Pokemon
     @name = name
     @type = type
     @db   = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+  end
+
+  def self.find(id, db)
+    pk_from_db = db.execute("SELECT * FROM pokemon WHERE id = (?);", id).flatten
+    self.new(id: id, name: pk_from_db[1], type: pk_from_db[2], db: db)
   end
 end
