ec6846b2550e95dbb7c925679d2569014a2eb49d
GusVilla303 <gusvilla303@gmail.com>
2017-10-10 11:55:07 -0600

    Add alter table with hp default of 60 to pokemon table

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
 
   attr_reader :id, :name, :type, :db
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp: nil)
     @id   = id
     @name = name
     @type = type
