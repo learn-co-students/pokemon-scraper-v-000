b692bad43f0f4dc7bbace2fc3ae4e720527329fd
github username <github email address>
2017-10-10 22:53:10 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,25 +1,28 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id=nil, pk_name=nil, pk_type=nil, db=nil)
+  def initialize(id=nil, name=nil, type=nil, hp=nil)
     @id = id
-    @name = pk_name
-    @type = pk_type
+    @name = name
+    @type = type
     @db = db
+    @hp = hp
   end
 
-  def self.save(pk_name, pk_type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", pk_name, pk_type)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
 
   end
 
   def self.find(id, db)
     pokemon_name_by_id = db.execute("SELECT * FROM pokemon WHERE pokemon.id= ?;", id).flatten
-    new_pokemon = Pokemon.new(pokemon_name_by_id[0], pokemon_name_by_id[1], pokemon_name_by_id[2])
-
+    # binding.pry
+    new_pokemon = Pokemon.new(pokemon_name_by_id[0], pokemon_name_by_id[1], pokemon_name_by_id[2], pokemon_name_by_id[3])
+    # binding.pry
   end
 
-  def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp= ?", hp)
+  def alter_hp(new_hp, db)
+    self.hp = new_hp
+    db.execute("UPDATE pokemon SET hp= ? WHERE name = ?", @hp, self.name)
   end
 end
