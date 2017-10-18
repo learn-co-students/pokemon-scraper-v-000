74c50ccca139a2a6ef954735009d39d1cc47eb93
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-18 20:19:19 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   @@all = []
 
   def initialize (attributes)
     @id = attributes[:id]
     @name = attributes[:name]
     @type = attributes[:type]
+    @hp = attributes[:hp]
     @db = attributes[:db]
     @@all << self
   end
@@ -15,21 +16,16 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);",name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);",name, type)
   end
 
   def self.find(id, db)
     pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    catch = Pokemon.new({id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], db:db})
-    catch
-
+    catch = Pokemon.new({id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], hp: pokemon_array[3],  db:db})
   end
-  # pokemon_array = []
-  # Pokemon.all.each do |pokemon|
-  #   pokemon_array << [new_id, name, type] if pokemon.id == id
-  #   binding.pry
-  # end
-  # pokemon_array
-  # binding.pry
+
+  def self.alter_hp(hp, db)
+    db.execute("UPDATE hp SET hp = ? WHERE name = ?", hp, self)
+  end
 
 end
