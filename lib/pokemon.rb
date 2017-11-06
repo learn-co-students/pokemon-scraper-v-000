a0a1169ffedf0b66555d368ca00a35612573d543
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-11-06 12:02:59 -0500

    all tests passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
 
   @@all = []
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :hp, :db
 
   def initialize(id:, name:, type:, hp: nil, db:)
       @id, @name, @type, @hp, @db = id, name, type, hp, db
@@ -33,6 +33,10 @@ class Pokemon
     #This next line I got too, except for #flatten, which is helpful.  The rest, I was lost.
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
   # def self.find(id, database_connection)
