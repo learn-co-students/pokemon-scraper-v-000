4cafd5465d3a44055659a0a966e604f0092b1714
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-11-06 11:21:36 -0500

    first two tests passing, working on third

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+  @@all = []
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id)
+    self.id = id
+    @@all << self
+    # #(id, name, type, db)
+    # self.id = id
+    # self.name = name
+    # self.type = type
+    # self.db = db
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, database_connection)
+    # pk = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    pk = database_connection.execute("SELECT id FROM pokemon WHERE id = ?", id)
+    Pokemon.new(pk)
+    #self.all[id - 1]
+  end
+
+
 end
