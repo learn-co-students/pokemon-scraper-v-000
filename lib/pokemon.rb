ad95c517bd16ce60f79bfc922645590f34b1401c
Caroline Shields <caroline.shields@amkor.com>
2017-12-06 20:01:17 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 attr_accessor :id, :name,:type, :db, :hp
 
   #ALTER TABLE pokemon ADD COLUMN hp INTEGER DEFAULT 60;
-  def initialize
+  def initialize(hp=nil)
     @db = db
     @id = id
     @name = name
@@ -11,21 +11,27 @@ attr_accessor :id, :name,:type, :db, :hp
     @hp = hp
   end
 
-  def self.save(name, type,db)
+  def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)",name, type)
+
   end
 
   def self.find(id_num,db)
-    result = db.execute("SELECT * from pokemon where id = ?",id_num).flatten
+    #binding.pry
+    result = db.execute("SELECT * from pokemon where id=?",id_num).flatten
+    #binding.pry
     pokemon = Pokemon.new
     pokemon.id = result[0]
     pokemon.name = result[1]
     pokemon.type = result[2]
-    pokemon.db = db
+    pokemon.hp = result[3]
+    pokemon
   end
 
-  def alter_hp(new_hp,id_num)
-    db.execute("UPDATE pokemon set hp=? where id =?)",new_hp,id_num)
+  def alter_hp(new_hp,db)
+    #binding.pry
+    db.execute("UPDATE pokemon set hp=? where id=?",new_hp,self.id)
+
   end
 
 end #ends Pokemon class
