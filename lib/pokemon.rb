f73e5c97f258188206abc095ea66182d2349476a
Caroline Shields <caroline.shields@amkor.com>
2017-12-07 18:00:12 +0000

    made comments for review

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,6 +11,11 @@ attr_accessor :id, :name,:type, :db, :hp
     @hp = hp
   end
 
+#SOLUTION - what's with the : after variables
+  # def initialize(id:, name:, type:, hp: nil, db:)
+  #   @id, @name, @type, @hp, @db = id, name, type, hp, db
+  # end
+
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)",name, type)
 
@@ -26,11 +31,16 @@ attr_accessor :id, :name,:type, :db, :hp
     pokemon.type = result[2]
     pokemon.hp = result[3]
     pokemon
-  end
+
+#SOLUTION: why the : after the variables
+#Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+   end
+
+
 
   def alter_hp(new_hp,db)
     #binding.pry
-    db.execute("UPDATE pokemon set hp=? where id=?",new_hp,self.id)
+    db.execute("UPDATE pokemon set hp=? where id=?",new_hp,self.id) #how is self.id working
 
   end
 
