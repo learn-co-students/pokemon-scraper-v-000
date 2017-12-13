1a4cdf3390ed51e1032308f38ba1c7f495d19db8
Morgan Fudge <mfudge92@gmail.com>
2017-12-13 06:12:26 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  def initialize(id = nil, name = nil, type = nil, db)
+    @db = db
+    @name = name
+    @type = type
+    @id = id
+  end
+
+  def self.save(name, type, db)
+    id = db.execute("select max(id) from pokemon")[0]
+    id = id.nil? ? id + 1 : 1
+    s = db.prepare("insert into pokemon (id, name, type) values (?, ?, ?)")
+    s.execute([id, name, type])
+  end
+
+  def self.find(id, db)
+    data = db.execute("select * from pokemon where id = ?", [id])[0]
+    new(id, data[1], data[2], db)
+  end
 end
