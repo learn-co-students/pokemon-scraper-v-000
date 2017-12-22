5b23cc7ef4a22213ab3d218ea192d1a3c49f4aa9
github username <github email address>
2017-12-22 15:22:14 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
 
     @@all = []
 
-    def initialize(id:, name:, type:, hp: nil, db:)
+    def initialize(id:, name:, type:, db:)
         @id = id
         @name = name
         @type = type
