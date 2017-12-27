30c3eed2752476ee5390214362f38034ded3278a
Jacqueline Lee <anginblu@gmail.com>
2017-12-27 18:53:17 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,12 +6,12 @@ class Pokemon
   @@all = []
 
 
-  def initialize(id=nil, name=nil, type=nil, db=nil, hp = 60)
+  def initialize(id: nil, name: nil, type: nil, hp: 60, db: nil)
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
-    @hp = 60
     @@all << self
 end
 
@@ -21,16 +21,16 @@ end
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);",name, type)
-    self.new(db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[0], db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[1], db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[2], db)
+    self.new(id: db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[0], name: db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[1], type: db.execute("SELECT * FROM pokemon WHERE name = ?", name).flatten[2], db: db)
   end
 
   def self.find(id, db)
     self.all.bsearch {|pokemon| pokemon.id == id}
   end
 
-  def BONUS
-    db.execute("ALTER TABLE pokemon ADD hp INTEGER;")
-    db.execute("INSERT INTO pokemon (hp) VALUES (?);", @hp)
+  def alter_hp(hp, db)
+    self.hp = hp
   end
+
 
 end
