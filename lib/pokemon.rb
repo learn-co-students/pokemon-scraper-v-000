9a2b08f40b098c4fb442d97633356bd96e567376
Sally Zasloff <szasloff@gmail.com>
2017-12-29 00:28:45 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db
+
+
+  def initialize (name, type, db)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+
+  def self.save (name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+
 end
