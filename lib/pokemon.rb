ceb74af04c13a560b1c48847ce0a7d5d1ac6a260
lizbur10 <liz.burton147@gmail.com>
2018-01-06 09:09:21 -0500

    required elements done (with some additional code that will be needed for bonus section)

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :hp, :db
+
+    def self.save(name, type, hp=nil, db)
+        db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?)",name, type, hp)
+    end
+
+    def self.find(id, db)
+        this_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+        new_pokemon = Pokemon.new(id: id, name: this_pokemon.first[1], type: this_pokemon.first[2], hp: this_pokemon.first[3], db: db)
+    end
+
+    def initialize(id:, name:, type:, hp: nil, db:)
+        @id = id
+        @name = name
+        @type = type
+        @hp = hp
+        @db = db
+    end
+
 end
