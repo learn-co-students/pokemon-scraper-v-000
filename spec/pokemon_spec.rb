06225a578893b854a4b19846f554fbb092fccdd1
goatcheese82 <nonsquishy@gmail.com>
2018-01-10 16:13:19 -0600

    Done.

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -40,6 +40,8 @@ describe "Pokemon" do
     end
   end
 
+
+  <<-DOC
   describe "BONUS" do
 
     before do
@@ -71,4 +73,5 @@ describe "Pokemon" do
       expect(Pokemon.find(2, @db).hp).to eq(0)
     end
   end
+  DOC
 end
