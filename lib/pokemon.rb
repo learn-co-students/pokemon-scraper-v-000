f5aafb9a2decba63087e27d6d0997480a6aa9882
James Ferguson <ncjamesferguson@gmail.com>
2018-01-17 18:27:46 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon
+
+  def initialize(name, type, db)
+    db.execute( "INSERT INTO pokemon (name, type) VALUES (?, ?)", [id, name])
+  end
+
+  def self.save
+  end
+
+  def self.find
+  end
+
 end
