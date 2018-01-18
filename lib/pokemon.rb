ccdb2dae6ce814ad63e2fb74c84492dd0ee91013
charityyoungblood <youngblood.charity@gmail.com>
2018-01-18 13:58:38 -0800

    added find method to pokemon.rb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,9 +15,15 @@ class Pokemon
   def self.save(name, type, db)
    # saves an instance of Pokemon class into the database 
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+    
   end 
   
   def self.find(id, db)
+   
+    pokemon_character = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    
+    self.new(id: pokemon_character[0], name: pokemon_character[1], type: pokemon_character[2], db: db)
+    
     # The find method creates a new Pokemon after selecting their row from the database by their id number
   end
   
