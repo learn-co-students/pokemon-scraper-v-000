f27e0e6d823dc0862d847834420245f17b847a7c
charityyoungblood <youngblood.charity@gmail.com>
2018-01-18 11:20:37 -0800

    added arguments to save method in pokemon.rb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,17 @@
 class Pokemon
   
   attr_accessor :id, :name, :type, :db
-  
+  @@all = []
   def initialize(id:, name:, type:, db:)
     # is initialized with arguments - name, type and db
     @name = name 
     @type = type
     @db = db
     @id = id
+  end 
+  
+  def save(name, type, db)
+    
   end 
   
 end
