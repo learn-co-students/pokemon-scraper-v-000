76fc5dfb1524aee7a1551281945b903d9e342dde
rh24 <ms.rebeccahong@gmail.com>
2018-01-19 12:31:56 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
 
   def self.find(id, db)
     pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    self.new(id: pokemon_from_db[0], name: pokemon_from_db[1], type: pokemon_from_db[2], db: db)
+    self.new(id: pokemon_from_db[0], name: pokemon_from_db[1], type: pokemon_from_db[2], db: db, hp: pokemon_from_db[3])
   end
   # What if array is ["Pokemon name", "Pokemon type"] and is missing an element?
 
@@ -21,7 +21,8 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def alter_hp(id, hp)
-    db.execute("UPDATE pokemon SET hp = ? WHERE pokemon.id = id", hp)
+  def alter_hp(updated_hp, db)
+    # binding.pry
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", updated_hp, self.id)
   end
 end
--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -1,4 +1,5 @@
 require_relative "spec_helper"
+require 'pry'
 
 describe "Pokemon" do
   before do
