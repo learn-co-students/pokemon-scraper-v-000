bd1aa2c0626cf5920ed1f04dba0b0bff18b6a5f8
Annette <micha232@umn.edu>
2018-01-22 19:51:50 +0000

    2 out of 3 tests complete

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+    
+    attr_accessor :name, :type, :db, :id
+    
+    @@all = []
+
+    def initialize(attributes)
+        attributes.each {|key, value| self.send(("#{key}="), value)}
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.save(pokename, poketype, db)
+    #   pokemon = db.prepare('INSERT INTO pokemon (name, type) VALUES (?, ?)', pokename, poketype)
+    #   pokemon.execute
+        db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', pokename, poketype)
+    end
+    
 end
