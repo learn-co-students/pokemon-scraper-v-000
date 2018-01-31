ee7d22074a0ad59663bd094fcd84baa027d98950
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-30 20:29:01 -0500

    Rspeclemented Pokemon.new and Pokemon.save

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        prepared_insert_statement =
+            db.prepare('INSERT INTO pokemon (name, type) VALUES (?, ?)')
+        prepared_insert_statement.execute(name, type)
+    end
 end
