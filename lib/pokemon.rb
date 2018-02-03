83e630391d44190dbe56cd0070953de222e0e784
Araba Taylor <araba.taylor20@gmail.com>
2018-02-03 16:14:48 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+@@all = []
+
+  def initialize(id:, name:, type:, db: db = nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(id, name, type)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (@name, @type);")
+  end
+
+  def find
+
+  end
 end
