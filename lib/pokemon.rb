bd96a65c86aa75e289c506c6fec0d6b2cb69a037
Autumn J <autumnkangas@gmail.com>
2018-02-03 15:17:54 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,11 +14,6 @@ class Pokemon
 		db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
 	end
 
-	def self.find(id_number, db)
-		poke_deets = db.execute("SELECT * FROM pokemon WHERE id=?", id_number).flatten
-		Pokemon.new(id: poke_deets[0], name: poke_deets[1], type: poke_deets[2], db: db)
-	end
-
 	def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
