37d89aec3979add9cd3113a97e34d6db3c046549
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-02-04 22:14:56 +0000

    Complete Solution Here (Previous commit was a Learn Save)

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon
 #=====================================
   attr_accessor :id, :name, :type, :hp, :db
 #=====================================  
-  def initialize(attr_hash) 
+  def initialize(attr_hash, hp = nil)
     attr_hash.each{|k, v| self.send(("#{k}="), v)}
-    @hp = 60
+    @hp = hp
   end
   
   def self.save(name, type, db)
@@ -13,11 +13,10 @@ class Pokemon
   
   def self.find(id, db) 
     row = db.execute("SELECT * FROM Pokemon WHERE id = ?", id).flatten
-    Pokemon.new({id: row[0], name: row[1], type: row[2], db: db})
+    Pokemon.new({id: row[0], name: row[1], type: row[2], db: db}, row[3])
   end
   
   def alter_hp(hp, db)
-    binding.pry
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
   end
 #=====================================
