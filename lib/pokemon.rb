5de8bd4addb2380490b8ac7747d7d800a7005dff
Katrina Brinson <kbrinso@gmail.com>
2018-02-16 23:22:43 +0000

    completed everything except bonuses

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,43 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
+  
+  @@all = []
   def initialize(name:, type:, db:, id:)
     @name = name
     @type = type
     @db = db
     @@id = id
+    @@all << self
   end
   
   def self.save(name, type, db)
     #@id += 1
-    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", [@@id,name,type])
+    @vals = db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", [@@id,name,type])
+    @vals.each{|item|
+      self.new(id: @@id, name: name, type: type)
+      #binding.pry
+    }
   end
   
+  def self.find(id, db)
+    db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id.to_i)
+    #binding.pry
+    #@id = id
+    @@all.detect{|i|
+      i.id = id
+    }
+  end
+  
+  # all.each do |cat|
+  # Cat.save(cat.name, cat.breed, cat.age, database_connection)
+  
+  # @@all.each{|item|
+      
+  #   }
+  # Cat.new
+  # Cat.save(cat.name, cat.breed, cat.age, database_connection)
+   
+  def self.all
+    @@all
+  end
 end
