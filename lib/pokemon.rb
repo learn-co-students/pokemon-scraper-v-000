a247b02891219c4a14e1458ba9d6c2839cfad5fb
Katrina Brinson <kbrinso@gmail.com>
2018-02-19 17:21:13 +0000

    stuck on second bonus question

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,47 +12,26 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    #@id += 1
     @vals = db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", [@id,name,type])
   end
   
   def self.find(id, db)
-    vals = db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id.to_i)
-    #binding.pry
-    #@id = id
+    vals = db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id)
     newpoke = self.new(id: vals[0][0], db: db, name: vals[0][1], type: vals[0][2])
-    # vals.each{|item|
-    #   self.new(id: @@id, name: vals[1], type: vals[2], hp:60)
-    #   #binding.pry
-    # }
     #found = @@all.detect{|i| i.id = id}
   end
-  
-  # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
-  # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
-  
-  # all.each do |cat|
-  # Cat.save(cat.name, cat.breed, cat.age, database_connection)
-  
-  # @@all.each{|item|
-      
-  #   }
-  # Cat.new
-  # Cat.save(cat.name, cat.breed, cat.age, database_connection)
    
   def self.all
     @@all
   end
   
   def alter_hp(amt, db)
+    found = @@all.detect{|i| i.id == 1}
+    #found = self.class.find(1, db)
     #binding.pry
-    stm = db.prepare("UPDATE pokemon SET hp = ? WHERE pokemon.id = ?;")
-    #[amt, self.id]
-    stm.bind_param amt, $hp;
-    stm.bind_param self.id, $pokemon.id;
-    stm.execute;
-    stm.close;
-    #self.hp = amt
+    db.execute("UPDATE pokemon SET hp = ? WHERE pokemon.id = 1;", amt)
+    #db.execute("UPDATE pokemon SET hp = ? WHERE pokemon.id = ?;", [0, self.id + 1])
+    found.hp = amt
     #binding.pry
   end
 end
