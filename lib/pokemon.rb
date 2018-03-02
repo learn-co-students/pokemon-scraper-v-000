f707644b0a42c5a6c67debcd89e185094b523e4b
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-02 20:35:59 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,10 +17,8 @@ class Pokemon
   end
 
   def self.find(id, db)
-    poks = db.execute("SELECT * FROM pokemon WHERE id = ?", [id])
-    poksinner = poks[0]
-    poksinner[0]
-    self.new(@id = poksinner[0], @name = poksinner[1], @type = poksinner[2], @hp = poksinner[3])
+    poks = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).flatten
+    self.new(@id = poks[0], @name = poks[1], @type = poks[2], @hp = poks[3])
   end
 
   def alter_hp(new_hp,db)
