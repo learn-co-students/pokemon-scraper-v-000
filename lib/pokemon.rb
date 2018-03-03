3bb4a84c978874057225c174e9d3aefb9ee3d86f
Chely Ho <adept_inept@yahoo.com>
2018-03-03 06:46:53 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,73 @@
-class Pokemon
+class Pokemon1    #orignal class that passes the first tests
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(name:, type:, db:, id: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE ID = ?
+    SQL
+
+    id, name, type = db.execute(sql, id.to_s).flatten
+    self.new(id: id, name: name, type: type, db: db)
+  end
+
+end
+
+class Pokemon    #bonus version
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(name:, type:, db:, id: nil, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE ID = ?
+    SQL
+
+    id, name, type = db.execute(sql, id.to_s).flatten
+    self.new(id: id, name: name, type: type, db: db)
+  end
+
+ def alter_hp
+
+ end
+  # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
+  # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
 end
