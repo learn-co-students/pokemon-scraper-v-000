01e061b78942f498e6778c8f7a6eae294562e255
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-05 18:05:00 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,10 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   def initialize(name: ,type:,id:, db:)
     @name=name
     @type=type
     @id=id
+
   end
 
   def self.save(name,type,db)
@@ -12,4 +13,7 @@ class Pokemon
   def self.find(id, db)
     Pokemon.new(name: "Pikachu",type:"electric",id:id,db:db)
   end
+  def alter_hp(new_hp, db)
+   db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+ end
 end
