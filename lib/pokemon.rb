fb93a531b2cf2e929532b6e3b6ef6b2d759c6174
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-06 20:53:12 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
     Pokemon.new(id:pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3],db: db)
   end
   
-  def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
-  end
+  #def alter_hp(hp, db)
+    #db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  #end
 end
