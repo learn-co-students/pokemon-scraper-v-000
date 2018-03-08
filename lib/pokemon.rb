8950687df34657b29accffa2e4e0565944980381
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-08 11:28:02 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
-attr_accessor :id, :name, :type, :db
+attr_accessor :id, :name, :type, :hp, :db
 
   def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
+    @hp = 60
     @db = db
   end
 
