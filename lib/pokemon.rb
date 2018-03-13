c65e5bea8dee7884ca9193b5448c06179dc6f0fe
Eden Mesfun <emesfun@gmail.com>
2018-03-13 19:52:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db, :id, :hp
+
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
+    pokemon << 60
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
+    #db.execute("ALTER TABLE pokemon ADD hp NOT NULL DEFAULT 60")
+  end
+
+  def alter_hp
+  end
+
 end
