d0a0c187f145a281edaa904f14057e0838c4fd40
Jay Bednar <ironviralmedia@gmail.com>
2018-03-13 18:02:16 +0000

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -8,7 +8,9 @@ require_relative "../lib/pokemon"
 require_relative "../lib/scraper"
 require_relative "sql_runner"
 
+
 @db = SQLite3::Database.new('../pokemon.db')
 @db.execute("DROP TABLE IF EXISTS pokemon;")
 @sql_runner = SQLRunner.new(@db)
 @sql_runner.execute_schema_migration_sql
+@sql_runner.execute_create_hp_column
