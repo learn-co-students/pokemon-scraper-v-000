ac97a62716bac35dea2eb21aeebca621bf6f3d13
Ariana Berger <ariana.berger@gmail.com>
2018-03-16 17:31:17 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,14 +1,15 @@
+require 'pry'
+
 class Pokemon
 
   attr_accessor :id, :name, :type, :db
 
-  @@all = []
-  def initialize(pokemon)
-    @id = pokemon.id
-    @name = pokemon.name
-    @type = pokemon.type
-    @db = pokemon.db
-    @@all << self
+  def initialize(id)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    # @@all << self
   end
 
   def self.save(name, type, db) #db is a class that is passed in
@@ -16,7 +17,19 @@ class Pokemon
     #into the ??
   end
 
-  def self.all
-    @@all
+  # def self.all
+  #   @@all
+  # end
+
+  def self.find(id, db)
+    # pokemon = db.execute("SELECT pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id).flatten[0]
+    # @@all.detect {|poke_obj| poke_obj.name = pokemon}
+    pokemon = db.execute("SELECT pokemon.id, pokemon.name, pokemon.type FROM pokemon WHERE pokemon.id = ?", id).flatten
+    new_poke = self.new(pokemon[0])
+    # binding.pry
+    new_poke.name = pokemon[1]
+    new_poke.type = pokemon[2]
+    new_poke.db = db
+    new_poke
   end
 end
