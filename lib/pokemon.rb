a39f2ea42c01bc765ec302bddbc3c62de9cd7ff4
Ariana Berger <ariana.berger@gmail.com>
2018-03-16 00:47:56 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,21 @@ class Pokemon
 
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id)
-    @name = name
-    @type = type
-    @db = db
+  @@all = []
+  def initialize(pokemon)
+    @id = pokemon.id
+    @name = pokemon.name
+    @type = pokemon.type
+    @db = pokemon.db
+    @@all << self
+  end
+
+  def self.save(name, type, db) #db is a class that is passed in
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type) #the last 2 values are what are passed
+    #into the ??
+  end
+
+  def self.all
+    @@all
   end
 end
