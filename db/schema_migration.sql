9cc11cbc5fcb6678e3cd780f643f1b519e1c8b63
Ariana Berger <ariana.berger@gmail.com>
2018-03-20 15:46:26 +0000

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -3,4 +3,7 @@ CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)
 =======
 
 
+<<<<<<< HEAD
+>>>>>>> 53cd647a4dc38f712d62912f9d0a568b7b49ee04
+=======
 >>>>>>> 53cd647a4dc38f712d62912f9d0a568b7b49ee04
