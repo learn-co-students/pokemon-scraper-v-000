014a5cbd8ed5211fae825b639f01f0dfcbb795e0
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 16:06:01 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,20 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
+  @@all = []
+
   def initialize(id:, name:, type:, db:)
     @name = name
     @type = type
     @db = db
-    save
+    @id = id
+    @@all << self
   end
 
-  def save
-    @id = id
+  def self.save(@id, @name, @type, @db)
+    #db = SQLite3::Database.new('db/pokemon.db')
+    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", @name, @type, )
+    end
   end
 
 end
