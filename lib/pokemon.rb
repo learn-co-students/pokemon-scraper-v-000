55e8c700e6152d8c859000dd00c7b5da502c3f54
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-21 14:49:00 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+    save
+  end
+
+  def save
+    @id = id
+  end
+
 end
