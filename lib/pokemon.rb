cc4938eb9086d74e553b6d60d372aaccf25c9fa1
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-24 02:04:06 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
   @@all = []
 
@@ -11,17 +11,21 @@ class Pokemon
     @@all << self
   end
 
-  def self.save(@id, @name, @type, @db)
-    #db = SQLite3::Database.new('db/pokemon.db')
-    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", @name, @type, )
-    end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type, )
   end
 
   def self.find(id_number, db)
-    pokemon_database_items = []
-    pokemon_database_items = db.execute("SELECT * FROM pokemon WHERE id = '#{id_number}'")
-    Pokemon.new(id: pokemon_database_items[0], name: pokemon_database_items[1], type: pokemon_database_items[2], db: db)
-    binding.pry
+    #pokemon_array = []
+    #binding.pry
+    pokemon_array = (db.execute("SELECT * FROM pokemon WHERE id = #{id_number}"))[0]
+    Pokemon.new(id: id_number, db: db, name: "#{pokemon_array[1]}", type: "#{pokemon_array[2]}")
+  end
+
+  def alter_hp(hp_update, db)
+  #binding.pry
+    self.hp = hp_update
+    db.execute("UPDATE pokemon SET hp = #{hp_update} WHERE name = '#{self.name}'")
   end
 
 end
