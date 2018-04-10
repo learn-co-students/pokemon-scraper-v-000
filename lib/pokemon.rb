88283c1cda9892560a90cc94052750d572bb7489
gbzhang6 <guibzhang@gmail.com>
2018-04-10 17:20:14 -0400

    Update pokemon.rb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,15 @@ class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
   def initialize (id:, name:, type:, hp:nil, db:)
-    @id, @name, @type, @hp, @db = id, name, type, hp, db
+    @id, 
+    @name, 
+    @type, 
+    @hp, 
+    @db = id, 
+    name, 
+    type, 
+    hp, 
+    db
   end
 
   def self.save(name, type, db)
