171a0bb665e9045829a8710cae0d5614ee791483
gbzhang6 <guibzhang@gmail.com>
2018-04-10 17:58:26 -0400

    Update pokemon.rb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,16 +1,12 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize (id:, name:, type:, hp:nil, db:)
-    @id, 
-    @name, 
-    @type, 
-    @hp, 
-    @db = id, 
-    name, 
-    type, 
-    hp, 
-    db
+  def initialize (hash)
+    @id = hash[:id],
+    @name = hash[:name],
+    @type = hash[:type], 
+    @hp = hash[:hp], 
+    @db = hash[:db]
   end
 
   def self.save(name, type, db)
@@ -19,7 +15,7 @@ class Pokemon
 
   def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+    Pokemon.new({id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db})
   end
 
   def alter_hp(new_hp, db)
