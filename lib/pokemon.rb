b37668734e9706cd4b2771af09a03f695183020d
osxgirl <osxgirl@mac.com>
2018-04-22 20:33:10 -0400

    //❤️

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db, :hp
+    
+    def initialize(id:, name:, type:, db:, hp: nil)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        @hp = hp
+    
+    end
+    
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type)
+                        VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")[0]
+        pikachu = Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: pokemon[3])
+    end
+
+   def alter_hp(hp, db)
+       db.execute("SELECT pokemon.id FROM pokemon WHERE pokemon.id = id")
+   end
+
 end
