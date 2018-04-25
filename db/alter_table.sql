f9558d43eb05b844bd845804c6466f6bfe92b52e
Vanessa Toro <vanetoro84@gmail.com>
2018-04-25 23:55:02 +0000

    Done.

---
+++ db/alter_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE pokemon ADD hp INTEGER;
+UPDATE pokemon SET hp = 60;
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
+# require 'pry'
 class Pokemon
+  attr_accessor  :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+
+  end
+
+  def alter_hp(new_hp)
+      db.execute("SELECT hp FROM pokemon WHERE = ?", @id)
+  end
+
 end
