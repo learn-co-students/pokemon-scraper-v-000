99a3981fd9cceef85a6c2b496580a0f9a1b400a3
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 01:35:05 +0000

    Done.

--- db/alter_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE pokemon ADD hp INTEGER;
-UPDATE pokemon SET hp = 60;
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,18 +10,19 @@ class Pokemon
     @hp = hp
   end
 
+
   def self.save(name, type, db)
-      db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
 
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
 
   end
 
-  def alter_hp(new_hp)
-      db.execute("SELECT hp FROM pokemon WHERE = ?", @id)
+  def alter_hp(new_hp, db)
+      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
   end
 
 end
