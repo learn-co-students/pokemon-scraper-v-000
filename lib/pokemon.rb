14ba561f7246368f4e70dcd504d06bf4ebcc5e1b
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-30 23:05:31 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,23 +1,13 @@
 require "pry"
 class Pokemon
-  attr_accessor :id, :name, :type, :db, :hp
+  attr_accessor :id, :name, :type, :db
 
-  def initialize (id:, name:, type:, hp: 60, db:)
+  def initialize (id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
-    @hp = hp
   end
-#  def initialize (attrs = {})
-  #  attrs.each { |k,v| instance_variable_set(k,v) }
-  # attrs.each { |name, value| instance_variable_set("@#{name}", value) }
-  #   @id = attrs.fetch(:id)
-  #   @name = attrs.fetch(:id)
-  #   @type = attrs.fetch(:id)
-  #   @db = attrs.fetch(:id)
-  #   @hp = attrs.fetch(:id, 60)
-  # end
 
   def self.save (name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
@@ -26,8 +16,6 @@ class Pokemon
   def self.find (id, db)
     db.results_as_hash = true
     results = db.execute("SELECT * FROM pokemon WHERE id=?",id)
-  #  binding.pry
     Pokemon.new(id: results[0]["id"], name: results[0]["name"], type: results[0]["type"], db: db)
-#      hp: results[0].has_key?("hp") ? results[0]['hp'] : nil)
   end
 end
