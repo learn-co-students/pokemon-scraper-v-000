bdd487df8651cf49388565e30ffa7858acdc0b0d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-04 14:02:41 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,10 +5,16 @@ class Pokemon
   @@all = []
   
   def initialize(db)
+    @db = db
   end
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?, ?)",name, breed, age)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
+  
+  def self.find(id) 
+    
+  end
+  
   
 end
