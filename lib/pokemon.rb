407ed6114bf2aa2efddf356509163bb0005c121e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-04 14:50:48 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,13 +6,18 @@ class Pokemon
   
   def initialize(db)
     @db = db
+    @id = id
+    @name = name
+    @type = type
   end
   
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
   
-  def self.find(id) 
+  def self.find(id, db) 
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = #{id}")
+     
     
   end
   
