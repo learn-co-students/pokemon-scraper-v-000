9771c46d2c69034c48edeb5ba021f07845e973d9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-04 14:00:18 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db, :id
+  
+  @@all = []
+  
+  def initialize(db)
+  end
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?, ?)",name, breed, age)
+  end
+  
 end
