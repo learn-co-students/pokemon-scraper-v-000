0bba18f03e880f6b99571191ada572fe8fdb077b
Lucy Wall <lhwall@gmail.com>
2018-05-14 19:52:52 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end 
+  
+  def self.save(name, type, db)
+  end
+  
 end
