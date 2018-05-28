0c219c881785c4e3ec8fde100bc4cd5d71d714d8
Emmy White <emmalinerosewhite@gmail.com>
2018-05-28 17:38:09 +0000

    Small progress.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+  attr_accessor
+  @@all = []
+
+
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+  
+  def self.save
+   
+  end
 end
