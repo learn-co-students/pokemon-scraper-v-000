ff66e97604d84130cf0ea7a45207decc3af3d5ad
Ron Sala <ron.sala@gmail.com>
2018-05-29 12:43:38 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,27 +20,9 @@ attr_accessor :id, :name, :type, :db
     SQL
   end
 
-  # def self.find(id, db) # undefined method `id' for nil:NilClass
-  # def self.find(:id, db) #syntax error, unexpected tSYMBEG, expecting ')' (SyntaxError)
-  # def self.find(id:, db) # syntax error, unexpected tIDENTIFIER
   def self.find(id, db)
-  # def self.find(id:, db:) # wrong number of arguments (given 2, expected 0)
-
-    # pok = db.execute("SELECT * FROM pokemon WHERE pokemon.id = id;")
-    # binding.pry
-    # id: = pok[0][0]
-    # n = pok[0][1]
-    # t = pok[0][2]
-    # new_pokemon = Pokemon.new(id:, name:, type:, db:) # syntax error, unexpected ',' (SyntaxError)
-    # new_pokemon = Pokemon.new # missing keywords: id, name, type, db
-    # new_pokemon = Pokemon.new(id: pok[0][0], name: pok[0][1], type: pok[0][2], db:)
-    # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     pokemon_row = db.execute("SELECT * FROM pokemon where pokemon.id = id")
-    pokemon_from_db = Pokemon.new(name:, type:, db:, id:)
-    pokemon_from_db.id = pokemon_row[0]
-    pokemon_from_db.name = pokemon_row[1]
-    pokemon_from_db.name = pokemon_row[2]
-    binding.pry
+    pokemon_from_db = Pokemon.new(name: pokemon_row[0][1], type: pokemon_row[0][2], db: db, id: pokemon_row[0][0])
     pokemon_from_db
   end
 end
