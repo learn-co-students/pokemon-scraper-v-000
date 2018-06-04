bbce5531294257808281efe30f27787d3d5a9fd5
Tatyana Scotce <tscotce@gmail.com>
2018-06-04 22:09:30 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
   def initialize(pokemon)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
   end
   
   def self.save(name, type, db)
@@ -20,7 +21,12 @@ class Pokemon
     pokemon.id = pk[0][0]
     pokemon.name = pk[0][1]
     pokemon.type = pk[0][2]
+    pokemon.hp = pk[0][3]
     pokemon
+  end
+  
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = '#{hp}' WHERE name = '#{name}'")
   end
   
 end
