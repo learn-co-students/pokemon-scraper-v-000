7f11c93a06579f560e6e3ec1ac8fe25767633371
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-06-05 01:18:27 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
   end
   
   def alter_hp(new_hp, db)
-    # binding.pry
+    binding.pry
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
     # binding.pry
   end
