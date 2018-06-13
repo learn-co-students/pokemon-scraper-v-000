7064a2042b02c4351519c8f55904c503eff393b2
Anu Khambete <anukhambete@gmail.com>
2018-06-13 17:57:58 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,4 +12,11 @@ attr_accessor :id, :name, :type, :db
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
+    Pokemon.new(id:pokemon[0],name:pokemon[1],type:pokemon[2],db:db)
+  end
+
+
 end
