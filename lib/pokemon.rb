8f9fa8eb3cfd538844508f2a6fcacaf44e2ac535
sgibson47 <sgibson47@gmail.com>
2018-06-14 13:08:41 -0500

    update the initialize method to accept an argument of hp that defaults to nil if not set

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
   end
 
   def self.save(name, type, db)
