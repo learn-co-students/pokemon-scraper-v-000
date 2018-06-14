80de12ae8afacbfd00428764b47dc70647425a6f
sgibson47 <sgibson47@gmail.com>
2018-06-14 12:36:23 -0500

    delete unnecessary code

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,6 @@ class Pokemon
     array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     array_2 = array[0]
     new_instance = self.new(id:array_2[0], name:array_2[1], type:array_2[2], db:db)
-    new_instance
   end
 
 end
