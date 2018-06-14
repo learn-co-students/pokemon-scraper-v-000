fb2547a8ac2332b61d25d0278b6d7de881aec279
sgibson47 <sgibson47@gmail.com>
2018-06-14 13:10:21 -0500

    update #find to create a new Pokemon object with an id, type, name AND hp

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
   def self.find( id, db)
     array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     array_2 = array[0]
-    new_instance = self.new(id:array_2[0], name:array_2[1], type:array_2[2], db:db)
+    new_instance = self.new(id:array_2[0], name:array_2[1], type:array_2[2], db:db, hp:array_2[3])
   end
 
   def alter_hp(new_hp, db)
