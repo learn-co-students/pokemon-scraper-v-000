648fab7d90f9c8c2e0b43cbc33ca4a7106c15fc1
sgibson47 <sgibson47@gmail.com>
2018-06-14 12:35:13 -0500

    drill farther into .execute result to access data

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,15 +2,21 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) 
+      VALUES (?, ?)", name, type)
   end
 
   def self.find( id, db)
     array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    new_instance = self.new(id:array[0], name:array[1], type:array[2], db:db)
+    array_2 = array[0]
+    new_instance = self.new(id:array_2[0], name:array_2[1], type:array_2[2], db:db)
     new_instance
   end
 
