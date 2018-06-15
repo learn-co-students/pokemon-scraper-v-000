a78a812e6eff6390a624dd350c451d96fea67f6d
taryn yaeger <tsyaeger@gmail.com>
2018-06-15 05:01:18 +0000

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,1 +1,13 @@
-CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+
+
+
+-- schema_migration.sql  creates pokemon table (FAILED - 1)
+--   has an autoincrementing id column (FAILED - 2)
+--   has a name column (FAILED - 3)
+--   has a type column (FAILED - 4)
+  
+  
+  CREATE TABLE IF NOT EXISTS pokemon (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      type TEXT);
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
+
+
+  # .initialize
+  #   is initialized with keyword arguments of name, type and db (FAILED - 5)
+  # .save
+  #   saves an instance of a pokemon with the correct id (FAILED - 6)
+  # .find
+  #   finds a pokemon from the database by their id number and returns a new Pokemon object (FAILED - 7)
+
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  @@all = []
+  
+  def initialize(id:, name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  
+  def self.save(name,type,db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name,type)
+  end
+  
+  
+  def self.find(find_id,db)
+      poke = db.execute("SELECT * FROM pokemon WHERE id = ?", find_id)
+      poke = poke.flatten
+      # binding.pry
+      self.new(id: poke[0], name: poke[1], type: poke[2], db: db)
+  end
+ 
+
+  
 end
