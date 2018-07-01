dbcf1582a203be0faeb6c866007e07d958dc58ac
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-01 18:55:11 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp: nil)
     self.id = id
     self.name = name 
     self.type = type 
     self.db = db
+    self.hp = hp
   end
   
   def self.save(name, type, db)
@@ -16,7 +17,12 @@ class Pokemon
     attributes_array = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
     name = attributes_array[1]
     type = attributes_array[2]
-    new(id: id, name: name, type: type, db: db)
+    hp = attributes_array[3]
+    new(id: id, name: name, type: type, db: db, hp: hp)
+  end
+  
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE name = ?", hp, self.name)
   end
   
 end
