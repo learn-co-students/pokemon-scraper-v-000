12bafc53af4ef27c2860f6c239a0a9483bf4f751
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 15:44:09 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,12 +3,12 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:, hp:)
+  def initialize(id:, name:, type:, db:, hp: nil)
     self.id = id
     self.name = name
     self.type = type
     self.db = db
-    self.hp = hp
+    self.hp = hp if hp
   end
 
   def alter_hp(hp, db)
@@ -22,7 +22,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    id, name, type = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    return self.new(id: id, name: name, type: type, db: db)
+    id, name, type, hp = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    return self.new(id: id, name: name, type: type, db: db, hp: hp)
   end
 end
