3b8ac4f26d17a41607d357c79b3171469ce4d8f6
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-29 13:53:28 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,6 +9,8 @@ class Pokemon
     @db = db
     @id = id
     @hp = hp
+    # more elegant:
+    # @id, @name, @type, @hp, @db = id, name, type, hp, db
   end
 
   def self.save(name, type, db)
@@ -16,17 +18,11 @@ class Pokemon
   end
 
   def self.find(id, db)
-    pokemon = db.execute("SELECT id, name, type, hp FROM pokemon WHERE pokemon.id = ?", [id])
-    pokemon = pokemon.flatten
-    name = pokemon[1]
-    type = pokemon[2]
-    hp = pokemon[3]
-    Pokemon.new(id: id, hp: hp, name: name, type: type, db: db)
+    pokemon = db.execute("SELECT id, name, type, hp FROM pokemon WHERE pokemon.id = ?", [id]).flatten
+    Pokemon.new(id: id, hp: pokemon[3], name: pokemon[1], type: pokemon[2], db: db)
   end
 
   def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE pokemon.id = ?", [new_hp, @id])
-    # @hp = new_hp
-    # @hp = db.execute("SELECT hp FROM pokemon WHERE pokemon.id = ?", [@id]).flatten.first
   end
 end
