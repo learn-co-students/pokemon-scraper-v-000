4f2d5612dfbcb21b989ce7b805e60d15d162aaaa
Madeline Stark <madeline.stark1@gmail.com>
2018-08-07 22:34:54 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+@@all = []
+
+ def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+ def self.save(name, type, db)
+   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+ end
+
+ def self.find(id, db)
+   found = db.execute("SELECT * FROM pokemon WHERE id = ?", id) #can't interpolate in sql 
+   lost = []
+   lost = found.flatten
+   Pokemon.new(id: lost[0], name: lost[1], type: lost[2], db: db) #need to be key value pairs
+ end
+
 end
