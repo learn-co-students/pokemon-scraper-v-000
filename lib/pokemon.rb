ed1c07a3882301771fcb279701468099b8478201
Kazumi Karbowski <kazkazinus@gmail.com>
2018-08-07 23:39:55 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db, :hp
+   
+    def initialize(id:, name:, type:, db:, hp: nil)
+        @id = id
+        @name = name
+        @type = type 
+        @db = db
+        @hp = hp
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon_row = db.execute("SELECT * FROM pokemon WHERE id = pokemon.id").flatten
+        p_name = pokemon_row[1]
+        p_type = pokemon_row[2]
+        p_hp = pokemon_row[4]
+        self.new(id: id, name: p_name, type: p_type, db: db, hp: p_hp)
+    end
+
+    def alter_hp(num, db)
+        db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", num, self.id)
+    end
+
 end
+
+
