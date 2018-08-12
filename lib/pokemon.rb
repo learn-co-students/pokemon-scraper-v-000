65292395fdef8495a91d740be47767525160f946
Emily J Cain <emilycain@ymail.com>
2018-08-12 11:18:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db, :hp  
   
-  def initialize(id:, name:, type:, hp:, db:)
+  def initialize(id:, name:, type:, hp: nil, db:)
     @id = id 
     @name = name
     @type = type
