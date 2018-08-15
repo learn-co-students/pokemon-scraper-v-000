f685c4c39a6ca5e45835a76e274cf13e8cddafb5
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-15 22:07:31 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+
+  database_connection = SQLite3::Database.new('db/pokemon.db')
+  @@all = []
+
+  def initialize(name, type, database)
+    @name = name
+    @type = type
+    @@all < self
+  end
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon(name, type) Values(name, type)")
+  end
+
 end
