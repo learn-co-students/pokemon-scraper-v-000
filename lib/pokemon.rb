bfbe08cd2cb231bd0830ecda84b1e54904abe4a2
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-16 22:50:41 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,21 +1,22 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :hp, :db
   database_connection = SQLite3::Database.new('db/pokemon.db')
   @@all = []
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, hp:nil, db:)
     @id = id
     @name = name
     @type = type
     @db = db
-    @@all << self
+    @hp = hp
+
   end
 
   def self.all
     @@all
   end
 
-  def self.save(name, type, db)
+  def self.save(name, type,  db)
     sql = "INSERT INTO pokemon(name, type) Values(?, ?)"
     db.execute(sql, name, type)
   end
@@ -23,7 +24,12 @@ class Pokemon
   def self.find(id, db)
     sql = "SELECT * FROM pokemon WHERE id = ?"
     row = db.execute(sql, id).flatten
-    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+    Pokemon.new(id: row[0], name: row[1], type: row[2], hp: row[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    sql = "UPDATE pokemone SET hp = ? WHERE id = ?"
+    db.execute(sql, new_hp, self.id)
   end
 
 
