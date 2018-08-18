9fc60ac09db9c8bb0c489436cc31a9d79f19b6dd
DMCatanach <dawn.catanach@gmail.com>
2018-08-18 16:39:50 -0600

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -12,3 +12,4 @@ require_relative "sql_runner"
 @db.execute("DROP TABLE IF EXISTS pokemon;")
 @sql_runner = SQLRunner.new(@db)
 @sql_runner.execute_schema_migration_sql
+@sql_runner.execute_create_hp_column
