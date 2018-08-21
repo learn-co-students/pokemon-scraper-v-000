7a1095d989887e1520e97867d800592417e4f892
Nate Dietz <nsdietz@gmail.com>
2018-08-20 22:13:16 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,11 +10,11 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
   end
 
   def self.find(row_id, db)
-    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id=#{row_id}")
+    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", row_id)
     pokemon_hash = {}
     pokemon_from_db.each do |value|
       pokemon_hash[:id] = value[0]
@@ -25,8 +25,8 @@ class Pokemon
     pokemon_object = self.new(pokemon_hash)
   end
 
-  def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp=#{hp} WHERE id=#{self.id};")
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
 
