dfb054960f14424cda92a85fc7bafbf1dfebb831
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-25 03:41:10 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+
+  @@all = []
+
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
