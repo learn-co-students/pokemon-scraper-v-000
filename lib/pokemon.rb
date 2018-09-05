39695bcf540bf8366a27d4596ed70df477db94e7
Ayana Bando <ayanaflax@yahoo.com>
2018-09-05 21:07:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
+require 'pry'
+
 class Pokemon
+  
+  attr_accessor :name, :type, :db, :id
+  
+  @@all = []
+  
+  def initialize(id)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.all
+    @@all << self
+  end
+  
+  def self.save(name, type, db)
+    binding.pry
+    @@all << save(name, type, db)
+    self.each do |pk|
+      self.save(pk.name, pk.type, pk.db, pk.id)
+    end
+  end
 end
