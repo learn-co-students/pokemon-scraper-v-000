13c61c89ae2ce3ff6efd7a3012c17b153b86f8c1
Ayana Bando <ayanaflax@yahoo.com>
2018-09-06 03:13:11 +0000

    alter table and build pokemon class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,29 +1,25 @@
-require 'pry'
-
 class Pokemon
-  
-  attr_accessor :name, :type, :db, :id
-  
-  def initialize(id)
+
+attr_accessor :id, :name, :type, :hp, :db
+
+  def initialize(id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
-   end
-  
+  end
+
   def self.save(name, type, db)
-     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
-  
+
   def self.find(id, db)
-    binding.pry 
-    pokemon = db.execute("SELECT * FROM Pokemon WHERE id = ?", [id]).first
-      self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
-      
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
-  
-  def alter_hp(new_hp = 60, db)
+
+  def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 end
-
