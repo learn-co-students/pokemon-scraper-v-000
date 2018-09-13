d8418b04085209e05e0769a2589288901677eb95
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-13 13:42:53 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,12 +15,11 @@ class Pokemon
   
   def self.find(id, db)
     pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = (?)", id).flatten
-    new_obj = self.new(id: id, name: pokemon_array[1], type: pokemon_array[2], db: db, hp: 60)
+    self.new(id: id, name: pokemon_array[1], type: pokemon_array[2], db: db, hp: pokemon_array[3])
   end
   
-  def alter_hp(hp, db)
-    db.execute("UPDATE pokemon SET hp = (?) WHERE id = (?)", self.hp = hp, id)
-    self
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = (?) WHERE id = (?)", new_hp, id)
   end
   
 end
