8229aa38441cf302b3fe7f5b9fdd43d0339fa78b
George  Pianka <gpianka1@gmail.com>
2018-10-02 11:35:22 +0000

    Removed Unnecessary Regex Option

--- bin/environment.rb
+++ bin/environment.rb
@@ -6,9 +6,9 @@ require 'pry'
 
 require_relative "../lib/pokemon"
 require_relative "../lib/scraper"
-require_relative "sql_runner"
+require_relative "./sql_runner"
 
-@db = SQLite3::Database.new('../pokemon.db')
+@db = SQLite3::Database.new('db/pokemon.db')
 @db.execute("DROP TABLE IF EXISTS pokemon;")
 @sql_runner = SQLRunner.new(@db)
 @sql_runner.execute_schema_migration_sql
