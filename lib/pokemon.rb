46edaa85c8f45a499e1fe6280096c0b1c35f00bf
Richard Cho <richardcho100@gmail.com>
2018-10-23 00:18:03 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = id")
-    Pokemon.new(id, db)
+    array = db.execute("SELECT * FROM pokemon WHERE id = id")
+    binding.pry
   end
 end
