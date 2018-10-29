03f7bed2008c0f30de067ab143c7de11880d4b74
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-29 23:25:09 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,10 +21,7 @@ class Pokemon
 
   def self.find(id, database_connection)
     id_name_type = database_connection.execute("SELECT * FROM pokemon WHERE pokemon.id = id").join(",").split(",")
-
-
     @@all.detect{|pokemon| pokemon.id == id_name_type[0].to_i && pokemon.name == id_name_type[1] && pokemon.type == id_name_type[2]}
-
   end
 
 
