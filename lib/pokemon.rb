d946d06a098ec496a93bcc276af3416f0c42c1ed
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-29 22:27:28 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+
+  attr_accessor :name, :id, :type, :db
+
+
+  def initialize(id, name, type, db)
+    @id = :id
+    @name = :name
+    @type = :type
+    @db = :db
+  end
+
+  def self.save
+  end
+
+  def self.find
+  end
+
+
 end
