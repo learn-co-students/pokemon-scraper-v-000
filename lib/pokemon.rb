e2b50b605759e7f845a16c725f90862a0b55f589
Harley Rayner <harleymteden@gmail.com>
2018-10-31 01:42:57 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,17 +27,16 @@ class Pokemon
   end
 
 #this might break if we add or delete columns from db at some point... how can i fix that?
-#finds the pokemon in a db with the given id, and creates a new pokemon object utilising the data maintained in the database
+#finds the pokemon in a db with the given id, and creates a new pokemon object utilising the data in the database
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    Pokemon.new(id: id, name: pokemon[0][1], type: pokemon[0][2], db: db, hp: 60)
+    Pokemon.new(id: id, name: pokemon[0][1], type: pokemon[0][2], db: db, hp: pokemon[0][3])
   end
 
 #finds the object with the name "pikachu" in our Pokemon class, changes pikachu's hp property
   def alter_hp(new_hp, db)
-    Pokemon.all.detect do |pokemon| pokemon.id == 1
-      pokemon.hp = new_hp
-    end
+      # binding.pry
+      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
 end
