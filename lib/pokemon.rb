be364722a7a5337ae58d9d55e23625c85f827277
Justin Kenney <justinkenney13@gmail.com>
2018-10-31 19:17:06 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES(?, ?)", name, type)
+    db.execute("INSERT INTO pokemon(name, type) VALUES(?, ?)", name, type)
   end
 
   def self.find(id, db)
@@ -19,8 +19,11 @@ class Pokemon
     Pokemon.new(id: var[0], name: var[1], type:var[2])
   end
 
-  def alter_hp(hp)
-    binding.pry
+  def alter_hp(hp, db)
+    # binding.pry
+    db.execute("UPDATE pokemon SET hp = #{hp}
+    WHERE name = #{name}")
+    # @hp = hp
 
   end
 
