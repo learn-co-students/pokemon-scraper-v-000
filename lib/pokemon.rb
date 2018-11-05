59b84bfe22b3e04165d8986b3dbdcf245bcd815c
leothebeagle <a.kosber@gmail.com>
2018-11-05 18:26:01 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,20 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :hp, :db
 
-  def initialize(id:, name:, type:, db:)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id, @name, @type, @db, @hp = id, name, type, db, hp
   end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
   end
 
-  def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    new_pokemon = Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], db: pokemon)
-    new_pokemon
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+  end
 end
-
-
-# [[1, "Pikachu", "electric"]]
