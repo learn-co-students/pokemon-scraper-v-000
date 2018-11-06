8ad3f15e81fac4a7608fd187853289ae13fb4eb7
Samantha brake <sammy.brake@gmail.com>
2018-11-06 18:04:15 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    id = @@all.count + 1
+    @@all << Pokemon.new(id: id, name: name, type: type, db: db)
+  end
+
+  def self.all
+    @@all
+  end
 end
