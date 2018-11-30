b3db733c9fba32177773e1d7b256104dd3db6ee6
Cheryl K <cherylkelson@icloud.com>
2018-11-30 00:17:55 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+
+
+
+  def initialize
+
+  end
+
+
+
+  def save
+
+  end
+
+
+
+  def find
+
+  end
+
+
 end
+
+#Your only job is to build out the methods to save and find pokemon in the database.
