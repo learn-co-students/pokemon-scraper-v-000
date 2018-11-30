b90021931ec7487abd5aa6faca6ca6049b277932
Cheryl K <cherylkelson@icloud.com>
2018-11-30 01:25:15 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,13 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
 
-
-
-  def initialize(id, name, type, db)
+  def initialize(id:, name:, type:, db:)
 #Pokemon .initialize is initialized with keyword arguments of id, name, type and db
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    #@@all << self
   end
-
-
-
-  def save
-Pokemon .save saves an instance of a pokemon with the correct id
-  end
-
-
-
-  def find
-Pokemon .find finds a pokemon from the database by their id number and returns a new Pokemon object
-  end
-
 
 end
-
-#Your only job is to build out the methods to save and find pokemon in the database.
