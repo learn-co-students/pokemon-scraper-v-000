4ac21256faa144c32af22b495327ac99d2b1154a
Cheryl K <cherylkelson@icloud.com>
2018-11-30 22:13:01 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,18 +2,21 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id:, name:, type:, db:)
-#Pokemon .initialize is initialized with keyword arguments of id, name, type and db
     @id = id
     @name = name
     @type = type
     @db = db
-    #@@all << self
+
   end
 
-  def self.find(id,db)
-      db.execute("SELECT pokemon WHERE id = #{{id}}
-      Pokemon.new(id)
+
+  def self.find(id_num, db)
+      # db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
+      # Pokemon.new.db(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+      db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
+      Pokemon.new(id: pokemon_info[0] , name: pokemon_info [1], type: pokemon_info[2], db: db)
   end
+
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES ('#{name}', '#{type}')")
