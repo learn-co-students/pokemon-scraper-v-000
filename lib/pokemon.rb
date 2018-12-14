b5fc000194b9488f7cfbc7563fbbb261d068671f
taisaoko <ttd3calvin@gmail.com>
2018-12-14 19:32:21 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :name, :type, :db, :id, :hp
 
-  def initialize(keywords)
+  def initialize(id:, name:, type:, db:)
 
   end
 
@@ -11,7 +11,7 @@ class Pokemon
 
   def self.find(num, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", num)
-    new_pokemon = self.new(pokemon)
+    new_pokemon = self.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2])
     new_pokemon.id = pokemon[0][0]
     new_pokemon.name = pokemon[0][1]
     new_pokemon.type = pokemon[0][2]
