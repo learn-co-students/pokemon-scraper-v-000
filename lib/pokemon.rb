d379284ea132d9320350314fd1ca92b54fee67c3
taisaoko <ttd3calvin@gmail.com>
2018-12-17 17:52:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,10 +12,6 @@ class Pokemon
   def self.find(num, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", num)
     new_pokemon = Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], hp: pokemon[0][3], db: db)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
     new_pokemon
   end
 
