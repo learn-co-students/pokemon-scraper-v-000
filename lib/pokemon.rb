148a3b590a4a3f4bbb2afc0c70349c7a37abc789
Ashley Wong <ashwong04@gmail.com>
2019-01-01 21:57:59 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+  
+  
+  
+  def initialize(id, name: nil, type: nil, db: db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
