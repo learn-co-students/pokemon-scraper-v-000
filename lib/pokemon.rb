95b6a8bcd792647f51dbb32f11a9ba425605ce1d
Brad Schrag <brad.schrag@gmail.com>
2019-01-06 03:01:31 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db, :hp
+  
+  def initialize (args)
+    args.each do |k, v|
+      self.send("#{k}=", v)
+    end
+  end
+  
+  def self.save(name, breed, db)
+    db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)", name, breed)
+  end
+  
+  def self.find(id, db)
+    values = db.execute("SELECT * FROM Pokemon WHERE id=?", id)[0]
+    hash = {id: values[0], name: values[1], type: values[2], db: db}
+    Pokemon.new(hash)
+  end
+  
+  def alter_hp(hp, db)
+    db.execute("UPDATE Pokemon SET hp=? WHERE id=?", hp, @id)
+    puts "hp= #{self.hp}"
+  end
 end
