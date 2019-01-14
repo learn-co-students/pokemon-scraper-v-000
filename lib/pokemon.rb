84a6a52a519f3b1d3090142df1832c61774c32e1
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-14 04:45:47 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon
 
-  attr_accessor :id, :name, :type, :hp, :db
+     attr_accessor :id, :name, :type, :hp, :db
 
 
   def initialize(id:, name:, type:, hp: nil, db:)
@@ -26,18 +26,10 @@ def self.find(id, db)
     Pokemon.new(id: pokemon_find[0], name: pokemon_find[1], type: pokemon_find[2], hp: pokemon_find[3], db: db)
 end
 
-# def alter_hp(new_hp, db)
-#     # db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-#     db.execute("SELECT hp, id FROM pokemon UPDATE SET hp = ? WHERE id = ?", new_hp, db)
-#   end
-# UPDATE table_name
-# SET column1 = value1, column2 = value2, ...
-# WHERE condition;
 
-def bonus(new_hp, db)
 
-  alter_round = db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id).flatten
-  Pokemon.new(id: alter_round[0], hp: alter_round)
-
+def bonus_round(new_hp, db)
+    alter_round = db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id).flatten
+    Pokemon.new(id: alter_round[0], hp: alter_round)
  end
 end
