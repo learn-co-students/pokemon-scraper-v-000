4c19f067e8a49a4b4ee998f5055095dcc0c7f59b
Molly Nelson <mollykathrynnelson@gmail.com>
2019-02-25 00:30:24 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
+require "pry"
+
 class Pokemon
+  attr_reader :id, :name, :type, :db, :hb
+  @@all =[]
+
+  def initialize (id, name, db, hb)
+      @id = id
+      @name = name
+      @db = db
+      @hb = hb
+      @@all <<S self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)" name, type)
+  end
+  
+
+
+
+
+
+
 end
