9b3dd3292d417480c1ff5e382b0e5decf465abdd
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-06 20:57:04 +0000

    WIP

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
-  attr_reader :id, :name, :type, :db
+  attr_reader :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp:)
     @id = id
     @name = name
     @type = type
+    @hp = 60
     @db = db
   end
 
@@ -15,5 +16,10 @@ class Pokemon
   def self.find(id, db)
     pikachu_from_db = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?",id).first
     new(id: pikachu_from_db[0], name: pikachu_from_db[1], type: pikachu_from_db[2], db:db )
+  end
+
+  #def self.alter_hp(new_hp, db)
+    #db.excute("UPDATE pokemon SET hp = ?", [num], [self.id])
+    #self.hp = num
   end
 end
