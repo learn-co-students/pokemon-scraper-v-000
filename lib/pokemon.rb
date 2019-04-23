3234d0a4a3093a559c6f86591530fee2f8d2ffad
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-23 15:56:04 +0000

    .find returning syntax error

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+
+  end
+
+  def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM ? WHERE id = ? LIMIT 1"
+
+    db.execute(sql, db, id).each do |row|
+      Pokemon.new(row[0], row[1], row[2], db)
+    end.first
+  end
+
 end
+#  pokemon
