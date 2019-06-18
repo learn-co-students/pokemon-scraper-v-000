b7582998b6606ccf0c0c1508a4d95f2ad6e4a754
Karen Flores <karen.ag.flores@gmail.com>
2019-06-18 21:46:36 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,12 +2,12 @@ class Pokemon
   require 'pry'
   attr_accessor :name, :id, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
     @db = db
-    #@hp = hp
+    @hp = hp
   end
 
   def self.save(name, type, db)
@@ -28,13 +28,13 @@ class Pokemon
     SQL
 
     pokemon = db.execute(sql, id).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
 
   def alter_hp(new_hp, db)
-    #sql = <<-SQL
-    #  UPDATE pokemon SET hp = ? WHERE id = ?
-    #SQL
-    #db.execute(sql, [new_hp, self.id])
+    sql = <<-SQL
+      UPDATE pokemon SET hp = ? WHERE id = ?
+    SQL
+    db.execute(sql, new_hp, self.id)
   end
 end
