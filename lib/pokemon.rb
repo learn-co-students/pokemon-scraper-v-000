c0333c3fe3129d8f24407447f2a325d0c0accda5
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-25 18:31:17 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
@@ -10,13 +11,47 @@ class Pokemon
     @id = id
   end
 
-  def save
+  def self.save(name, type, db)
     sql = <<-SQL
       INSERT INTO pokemon (name, type)
-      VALUES (?, ?) WHERE id = ?
+      VALUES (?, ?)
+
     SQL
 
-    DB[:conn].execute(sql, name, type)
+    db.execute(sql, name, type)
   end
+
+  # def self.find(id, db)
+  #   sql = <<-SQL
+  #   SELECT * FROM pokemon WHERE id = ?
+  #   SQL
+  #   x = db.execute(sql, id)
+  #   x = x.flatten
+  #   Pokemon.new(x)
+  #   x.id =
+  #
+  # end
+
+  def self.find(id, db)
+   sql = <<-SQL
+     SELECT * FROM pokemon WHERE id = (?);
+   SQL
+   pokemon = db.execute(sql, [id]).flatten
+   Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
+  end
+  # def self.find(id, db)
+  #   sql = <<-SQL
+  #   SELECT * FROM pokemon WHERE id = ?
+  #   SQL
+  #   x = db.execute(sql,id)
+  #   x = x.flatten
+  #   Pokemon.new(id, x[1], x[2], x[3])
+
+    # id = x[0]
+    # name = x[1]
+    # type = x[2]
+    # db = x[3]
+    # return x
+
 
 end
