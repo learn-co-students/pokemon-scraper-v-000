b8982f0655bb39a011024c47f250fa72889b399d
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-25 03:43:23 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+
+  def initialize(id:, name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?) WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, name, type)
+  end
+
 end
