7c3af1042936846797faa5eda31948a8aaea92fb
Charles Okwechime <okwechimeamechi@gmail.com>
2019-12-31 01:37:27 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,6 @@ class Pokemon
       new_pokemon_id = pokemon[0][0]
       new_pokemon_name = pokemon[0][1]
       new_pokemon_type = pokemon[0][2]
-      new_pokemon = self.new({id: new_pokemon_id, name: new_pokemon_name, type: new_pokemon_type, db: db})
-      new_pokemon
+      self.new({id: new_pokemon_id, name: new_pokemon_name, type: new_pokemon_type, db: db})
     end
 end
