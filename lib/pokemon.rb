4882b5be85e1448af0d2e10d4abf93e488d49184
ortufte <ortufte@gmail.com>
2020-01-06 16:03:04 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+    attr_accessor :name, :type
+    attr_reader :id, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def save
+        DB[:conn].execute("INSERT INTO pokemon (name, type), VALUES (?, ?)", [name, type])
+    end
+
+    def find
+    end
+
 end
