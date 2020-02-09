6d835c8a084d26c6ca5bcea5b90a7e3242d65f01
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-09 01:37:06 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,47 @@
 class Pokemon
+attr_accessor :name, :type, :db, :id
+
+def initialize(id:nil, name:, type:, db:)
+  @id = id
+  @name = name
+  @type = type
+  @db = db
+end
+
+def self.create
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS pokemon (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      type TEXT
+    )
+  SQL
+@db.execute(sql)
+end
+
+def self.save(name, type, db)
+  sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?, ?)
+  SQL
+db.execute(sql, name, type)
+@id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+end
+
+def self.delete
+  sql = <<-SQL
+    DROP TABLE pokemon
+  SQL
+@db.execute(sql)
+end
+
+def self.find(id_num, db) #finds a pokemon from the database by their id number and returns a new Pokemon object (FAILED - 1)
+  sql = <<-SQL
+      SELECT * FROM pokemon
+      WHERE id =?
+  SQL
+  row = db.execute(sql, id_num).flatten
+  self.new(id: row[0], name: row[1], type: row[2], db: db)
+end
+
 end
