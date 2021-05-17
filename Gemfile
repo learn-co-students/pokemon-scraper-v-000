2a8700d88fe82d14faf3a48db82428775498052c
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-17 13:27:09 -0600

    Done.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 
 # Scraping
-gem 'nokogiri'
+
 
 # Database
 gem 'sqlite3', '~>1.3.6'
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    
+    def initialize(id: , name: , type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+       sql = "INSERT INTO pokemon (name, type) VALUES (?, ?);"
+       db.execute(sql, name, type)
+    end 
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        entry_array = db.execute(sql, id).flatten
+        pokemon = self.new(id: entry_array[0], name: entry_array[1], type: entry_array[2], db: db)
+    end 
 end
